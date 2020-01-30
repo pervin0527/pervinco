2a6b2793e5bf30b242ff0455c907bfdb53084f41
pervin0527 <zfbtldnz@gmail.com>
2020-01-30 16:23:12 +0900

    latest update 1.30

--- exp/mobilenet.py
+++ exp/mobilenet.py
@@ -8,10 +8,17 @@ import tensorflow as tf
 import random
 
 tf.enable_eager_execution()
-AUTOTUNE = tf.data.experimental.AUTOTUNE
-data_root = pathlib.Path('/home/barcelona/pervinco/datasets/four_shapes/train')
-print(data_root)
 
+IMG_HEIGHT, IMG_WIDTH = 192, 192
+BATCH_SIZE = 64
+EPOCHS = 10
+STEPS_PER_EPOCHS = 3
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+data_root = pathlib.Path('/home/barcelona/pervinco/datasets/face_gender_glass/train')
+saved_path = '/home/barcelona/pervinco/exp/weights/'
+model_name = 'test_face'
+print(data_root)
 
 all_image_paths = list(data_root.glob('*/*'))
 all_image_paths = [str(path) for path in all_image_paths]
@@ -20,7 +27,8 @@ image_count = len(all_image_paths)
 print(image_count)
 
 label_names = sorted(item.name for item in data_root.glob('*/') if item.is_dir())
-label_to_index = dict((name, index) for index,name in enumerate(label_names))
+print('labels : ', label_names, 'label_num : ', len(label_names))
+label_to_index = dict((name, index) for index, name in enumerate(label_names))
 
 
 all_image_labels = [label_to_index[pathlib.Path(path).parent.name]
@@ -28,16 +36,24 @@ all_image_labels = [label_to_index[pathlib.Path(path).parent.name]
 
 
 def preprocess_image(image):
-  image = tf.image.decode_jpeg(image, channels=3)
-  image = tf.image.resize(image, [192, 192])
-  image /= 255.0  # normalize to [0,1] range
+    image = tf.image.decode_jpeg(image, channels=3)
+    image = tf.image.resize(image, [IMG_HEIGHT, IMG_WIDTH])
+    image /= 255.0  # normalize to [0,1] range
 
-  return image
+    return image
+
 
 def load_and_preprocess_image(path):
-  image = tf.read_file(path)
-  return preprocess_image(image)
+    image = tf.io.read_file(path)
+    return preprocess_image(image)
 
+
+def load_and_preprocess_from_path_label(path, label):
+    return load_and_preprocess_image(path), label
+
+
+def change_range(image, label):
+    return 2*image-1, label
 
 
 path_ds = tf.data.Dataset.from_tensor_slices(all_image_paths)
@@ -49,13 +65,7 @@ image_label_ds = tf.data.Dataset.zip((image_ds, label_ds))
 
 ds = tf.data.Dataset.from_tensor_slices((all_image_paths, all_image_labels))
 
-def load_and_preprocess_from_path_label(path, label):
-  return load_and_preprocess_image(path), label
-
 image_label_ds = ds.map(load_and_preprocess_from_path_label)
-image_label_ds
-
-BATCH_SIZE = 32
 
 ds = image_label_ds.shuffle(buffer_size=image_count)
 ds = ds.repeat()
@@ -63,15 +73,10 @@ ds = ds.batch(BATCH_SIZE)
 ds = ds.prefetch(buffer_size=AUTOTUNE)
 
 
-mobile_net = tf.keras.applications.MobileNetV2(input_shape=(192, 192, 3), include_top=False)
-mobile_net.trainable=False
-
-
-def change_range(image,label):
-  return 2*image-1, label
+mobile_net = tf.keras.applications.MobileNetV2(input_shape=(IMG_HEIGHT, IMG_WIDTH, 3), include_top=False)
+mobile_net.trainable = True
 
 keras_ds = ds.map(change_range)
-
 
 # The dataset may take a few seconds to start, as it fills its shuffle buffer.
 image_batch, label_batch = next(iter(keras_ds))
@@ -83,17 +88,18 @@ feature_map_batch = mobile_net(image_batch)
 model = tf.keras.Sequential([
   mobile_net,
   tf.keras.layers.GlobalAveragePooling2D(),
-  tf.keras.layers.Dense(len(label_names), activation = 'softmax')])
+  tf.keras.layers.Dense(len(label_names), activation='softmax')])
 
 
-model.compile(optimizer=tf.train.AdamOptimizer(),
-              loss=tf.keras.losses.sparse_categorical_crossentropy,
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
               metrics=["accuracy"])
 
 
-steps_per_epoch=tf.ceil(len(all_image_paths)/BATCH_SIZE).numpy()
-steps_per_epoch
+# steps_per_epoch = tf.ceil(len(all_image_paths)/BATCH_SIZE).numpy()
 
-model.fit(ds, epochs=5, steps_per_epoch=3)
+model.fit(keras_ds, epochs=EPOCHS, steps_per_epoch=image_count // BATCH_SIZE)
+
+model.save(saved_path + model_name + '.h5')
 
 
