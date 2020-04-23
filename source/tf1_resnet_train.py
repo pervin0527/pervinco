7dc361884a3710e8c58f81e43862f8c7414ae9e0
pervinco <zfbtldnz@gmail.com>
2020-04-23 17:14:33 +0900

    latest update 2020.04.23

--- source/tf1_resnet_train.py
+++ source/tf1_resnet_train.py
@@ -15,24 +15,35 @@ from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras import optimizers
 from tensorflow.python.keras.callbacks import EarlyStopping, ModelCheckpoint
 from tensorflow.python.keras.layers import Dense
+from keras.backend.tensorflow_backend import set_session
+
+
+def set_gpu_option(which_gpu, fraction_memory):
+    config = tf.ConfigProto()
+    config.gpu_options.per_process_gpu_memory_fraction = fraction_memory
+    config.gpu_options.visible_device_list = which_gpu
+    set_session(tf.Session(config=config))
+
+set_gpu_option("0", 0.8)
+
 
 '''
 parameter values  
 '''
-dataset_name = 'beverage'
+dataset_name = 'cu50'
 model_name = dataset_name
-train_dir = '/home/barcelona/pervinco/datasets/' + dataset_name + '/train'
-valid_dir = '/home/barcelona/pervinco/datasets/' + dataset_name + '/valid'
+train_dir = '/data/backup/pervinco_2020/pervinco/datasets/' + dataset_name + '/train5'
+valid_dir = '//data/backup/pervinco_2020/pervinco/datasets/' + dataset_name + '/valid5'
 NUM_CLASSES = len(glob.glob(train_dir + '/*'))
 
 CHANNELS = 3
 IMAGE_RESIZE = 224
 # EARLY_STOP_PATIENCE must be < NUM_EPOCHS
-NUM_EPOCHS = 10
-EARLY_STOP_PATIENCE = 5
-BATCH_SIZE = 32
+NUM_EPOCHS = 30
+EARLY_STOP_PATIENCE = 3
+BATCH_SIZE = 16
 
-saved_path = '/home/barcelona/pervinco/model/'
+saved_path = '/data/backup/pervinco_2020/pervinco/model/'
 time = datetime.datetime.now().strftime("%Y.%m.%d_%H:%M") + '_keras'
 weight_file_name = '{epoch:02d}-{val_acc:.2f}.hdf5'
 
@@ -47,18 +58,18 @@ Out intention in this kernel is Transfer Learning by using ResNet50 pre-trained 
 i.e., the xyz_tf_kernels_NOTOP.h5 weights... 
 Use this weights as initial weight for training new layer using train images
 '''
-resnet_weights_path = '/home/barcelona/pervinco/source/weights/resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5'
+resnet_weights_path = '/data/backup/pervinco_2020/pervinco/source/weights/resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5'
 # resnet_weights_path = '/home/barcelona/다운로드/resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5'
 
 model = Sequential()
 model.add(ResNet50(include_top=False, pooling='avg', weights=resnet_weights_path))
-model.add(Dense(NUM_CLASSES, activation='softmax'))
+model.add(Dense(NUM_CLASSES, activation='sigmoid'))
 model.layers[0].trainable = True
 model.summary()
 
 optimizer = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 # optimizer = optimizers.Adam()
-model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
 
 '''
 image data generator define
--- tensorflow_tutorial.md
+++ tensorflow_tutorial.md
@@ -55,3 +55,31 @@
   
 - [EfficientDet](https://github.com/signatrix/efficientdet)
     - [efficientnet-pytorch](https://github.com/lukemelas/EfficientNet-PyTorch) 
+    
+    
+
+## Multi Label Classification
+ - [Multi label classification](https://www.analyticsvidhya.com/blog/2019/04/build-first-multi-label-image-classification-model-python/)
+  
+   - Multiclass classification means a classification task with more than two classes; e.g., classify a set of images of fruits which may be oranges, apples, or pears. Multiclass classification makes the assumption that each sample is assigned to one and only one label: a fruit can be either an apple or a pear but not both at the same time.
+
+
+   - Multilabel classification assigns to each sample a set of target labels. This can be thought as predicting properties of a data-point that are not mutually exclusive, such as topics that are relevant for a document. A text might be about any of religion, politics, finance or education at the same time or none of these.
+
+   #### reference
+   - https://godatadriven.com/blog/
+   - https://tykimos.tistory.com/13
+   - https://godatadriven.com/blog/keras-multi-label-classification-with-imagedatagenerator/
+   - https://www.tensorflow.org/guide/keras/functional#all_models_are_callable_just_like_layers
+   - https://github.com/KerasKorea/KEKOxTutorial/blob/master/16_Ensembling%20ConvNets%20using%20Keras.md
+   - https://dailyheumsi.tistory.com/111https://www.quora.com/What-is-the-difference-between-multilabel-and-multiclass-classification
+   - https://m.blog.naver.com/PostView.nhn?blogId=nomadgee&logNo=220819616738&proxyReferer=https:%2F%2Fwww.google.com%2F
+   - https://hwiyong.tistory.com/41
+
+## Ensemble  
+- [Tensorflow keras Ensemble](https://www.tensorflow.org/guide/keras/functional)
+    #### reference
+   - https://towardsdatascience.com/ensembling-convnets-using-keras-237d429157eb
+   - https://hwiyong.tistory.com/100
+   - https://www.kaggle.com/mgiraygokirmak/keras-cnn-multi-model-ensemble-with-voting/data
+
