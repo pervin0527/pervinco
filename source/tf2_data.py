eca6e7bf4e8bfc7bda907a63ab75bc4aa53d48ef
pervinco <zfbtldnz@gmail.com>
2020-05-19 10:38:28 +0900

    latest update 2020.05.19

--- source/tf2_data.py
+++ source/tf2_data.py
@@ -2,6 +2,8 @@ import tensorflow as tf
 from tensorflow import keras
 import pathlib
 import random
+import os
+import datetime
 import time
 
 AUTOTUNE = tf.data.experimental.AUTOTUNE
@@ -27,6 +29,7 @@ def basic_processing(ds_path, is_training):
     labels_len = len(labels)
     labels = dict((name, index) for index, name in enumerate(labels))
     labels = [labels[pathlib.Path(path).parent.name] for path in images]
+    labels = tf.keras.utils.to_categorical(labels, num_classes=labels_len, dtype='float32')
 
     return images, labels, len_images, labels_len
 
@@ -40,13 +43,14 @@ def preprocess_image(image):
 
 def load_and_preprocess_image(path):
     image = tf.io.read_file(path)
+    
     return preprocess_image(image)
 
 def make_tf_dataset(images, labels):
     image_ds = tf.data.Dataset.from_tensor_slices(images)
     image_ds = image_ds.map(load_and_preprocess_image, num_parallel_calls=AUTOTUNE)
 
-    lable_ds = tf.data.Dataset.from_tensor_slices(tf.cast(labels, tf.int64))
+    lable_ds = tf.data.Dataset.from_tensor_slices(tf.cast(labels, tf.float32))
 
     image_label_ds = tf.data.Dataset.zip((image_ds, lable_ds))
 
@@ -54,16 +58,28 @@ def make_tf_dataset(images, labels):
 
 
 if __name__ == "__main__":
-    train_dataset_path = '/data/backup/pervinco_2020/datasets/cu50/train5'
-    valid_dataset_path = '/data/backup/pervinco_2020/datasets/cu50/valid5'
+    dataset_name = 'cu50'
+    train_dataset_path = '/data/backup/pervinco_2020/datasets/' + dataset_name + '/train5'
+    valid_dataset_path = '/data/backup/pervinco_2020/datasets/' + dataset_name + '/valid5'
 
     train_images, train_labels, train_images_len, train_labels_len = basic_processing(train_dataset_path, True)
     valid_images, valid_labels, valid_images_len, valid_labels_len = basic_processing(valid_dataset_path, False)
 
     BATCH_SIZE = 32
     IMG_SIZE = 224
+    NUM_EPOCHS = 30
+    EARLY_STOP_PATIENCE = 3
     TRAIN_STEP_PER_EPOCH = tf.math.ceil(train_images_len / BATCH_SIZE).numpy()
     VALID_STEP_PER_EPOCH = tf.math.ceil(valid_images_len / BATCH_SIZE).numpy()
+
+    saved_path = '/data/backup/pervinco_2020/model/'
+    time = datetime.datetime.now().strftime("%Y.%m.%d_%H:%M") + '_keras'
+    weight_file_name = '{epoch:02d}-{val_accuracy:.2f}.hdf5'
+
+    if not(os.path.isdir(saved_path + dataset_name + '/' + time)):
+        os.makedirs(os.path.join(saved_path + dataset_name + '/' + time))
+    else:
+        pass
 
     train_ds = make_tf_dataset(train_images, train_labels)
     valid_ds = make_tf_dataset(valid_images, valid_labels)
@@ -73,9 +89,9 @@ if __name__ == "__main__":
     valid_ds = valid_ds.repeat().batch(BATCH_SIZE)
     valid_ds = valid_ds.prefetch(1)
 
-    base_model = keras.applications.xception.Xception(input_shape=(224, 224, 3),
-                                                  weights="imagenet",
-                                                  include_top=False)
+    base_model = keras.applications.xception.Xception(input_shape=(IMG_SIZE, IMG_SIZE, 3),
+                                                      weights="imagenet",
+                                                      include_top=False)
     avg = tf.keras.layers.GlobalAveragePooling2D()(base_model.output)
     output = tf.keras.layers.Dense(train_labels_len, activation="softmax")(avg)
     model = tf.keras.Model(inputs=base_model.input, outputs=output)
@@ -84,9 +100,14 @@ if __name__ == "__main__":
         layer.trainable = True
 
     optimizer = tf.keras.optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-    model.compile(loss="sparse_categorical_crossentropy", optimizer=optimizer, metrics=["accuracy"])
+    model.compile(loss="categorical_crossentropy", optimizer=optimizer, metrics=["accuracy"])
 
     cb_early_stopper = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=3)
+    cb_checkpointer = tf.keras.callbacks.ModelCheckpoint(filepath=saved_path + dataset_name + '/'
+                                                                  + time + '/' + weight_file_name,
+                                                                  monitor='val_accuracy',
+                                                                  save_best_only=True,
+                                                                  mode='auto')
 
     history = model.fit(train_ds,
                         epochs=10,
@@ -95,7 +116,6 @@ if __name__ == "__main__":
                         validation_data=valid_ds,
                         validation_steps=VALID_STEP_PER_EPOCH,
                         verbose=1,
-                        callbacks=[cb_early_stopper]
-                        )
+                        callbacks=[cb_early_stopper, cb_checkpointer])
 
-    model.save('/data/backup/pervinco_2020/model/test_model/tf_data_test_model')
+    model.save(saved_path + dataset_name + '/' + time + '/' + dataset_name + '.h5')
