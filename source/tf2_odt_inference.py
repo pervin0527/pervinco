6e4614e3db77663940bc008786425908305ab114
pervinco <zfbtldnz@gmail.com>
2020-09-03 15:45:19 +0900

    latest update 2020.09.03

--- source/tf2_odt_inference.py
+++ source/tf2_odt_inference.py
@@ -17,8 +17,6 @@ from datetime import datetime
 
 
 def load_image_into_numpy_array(path):
-    # img_data = tf.io.gfile.GFile(path, 'rb').read()
-    # image = Image.open(BytesIO(img_data))
     image = cv2.imread(path)
     (im_width, im_height) = image.size
     return np.array(image.getdata()).reshape((im_height, im_width, 3)).astype(np.uint8)
