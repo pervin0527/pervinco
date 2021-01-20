f3d7ab03d72babb56887fa46b194b044ac1b646d
pervin0527 <zfbtldnz@gmail.com>
2021-01-20 14:24:59 +0900

    updated 2021.01.20

--- source/tf2.3_EfficientNet_ver3.py
+++ source/tf2.3_EfficientNet_ver3.py
@@ -1,12 +1,11 @@
-import os, cv2, datetime, pathlib, random, argparse
-import pandas as pd
+import os, datetime, argparse, pathlib, random, cv2
 import numpy as np
-import matplotlib.pyplot as plt
 import tensorflow as tf
+import albumentations as A
 
+from matplotlib import pyplot as plt
+from functools import partial
 from sklearn.model_selection import train_test_split
-from tqdm import tqdm
-tqdm.pandas()
 
 # GPU setup
 gpus = tf.config.experimental.list_physical_devices('GPU')
@@ -27,80 +26,84 @@ else:
     except RuntimeError as e:
         print(e)
 
-
 # Load data & Set hyper-parameters
 AUTO = tf.data.experimental.AUTOTUNE
 EPOCHS = 1000
 BATCH_SIZE = 32 * strategy.num_replicas_in_sync
 IMG_SIZE = 224
 
-
-def preprocess_image(images, label=None):
-    image = tf.io.read_file(images)
-    image = tf.image.decode_jpeg(image, channels=3)
-    image = tf.image.resize(image, [IMG_SIZE, IMG_SIZE])
-    image = tf.keras.applications.efficientnet.preprocess_input(image)
-
-    if label is None:
-        return image
-
-    else:
-        return image, label
+transforms = A.Compose([A.Resize(224, 224, p=1),
+                        A.HorizontalFlip(p=0.4),
+                        A.VerticalFlip(p=0.3),
+                        A.Blur(p=0.1),
+    
+                        A.OneOf([
+                            A.RandomContrast(p=0.5, limit=(-0.5, 0.3)),
+                            A.RandomBrightness(p=0.5, limit=(-0.2, 0.3))
+                        ], p=0.5)
+                ])
 
 
-def make_tf_dataset(ds_path, is_train):
+def basic_processing(ds_path):
     ds_path = pathlib.Path(ds_path)
 
     images = list(ds_path.glob('*/*'))
     images = [str(path) for path in images]
-    total_images = len(images)
-
-    if is_train:
-        random.shuffle(images)
+    images_len = len(images)
 
     labels = sorted(item.name for item in ds_path.glob('*/') if item.is_dir())
-    num_labels = len(labels)
+    labels_len = len(labels)
     labels = dict((name, index) for index, name in enumerate(labels))
     labels = [labels[pathlib.Path(path).parent.name] for path in images]
-    labels = tf.keras.utils.to_categorical(labels, num_classes=num_labels, dtype='float32')
+    labels = tf.keras.utils.to_categorical(labels, num_classes=labels_len, dtype='float32')
+
+    return images, labels, labels_len
+
+
+def aug_fn(image, img_size):
+    data = {"image":image}
+    aug_data = transforms(**data)
+    aug_img = aug_data["image"]
+    aug_img = tf.cast(aug_img, tf.float32)
+    
+    return aug_img
+
+
+def process_data(image, label, img_size):
+    aug_img = tf.numpy_function(func=aug_fn, inp=[image, img_size], Tout=tf.float32)
+
+    return aug_img, label
+
+
+def preprocess_image(images, label=None):
+    image = tf.io.read_file(images)
+    image = tf.image.decode_jpeg(image, channels=3)
+    image = tf.keras.applications.efficientnet.preprocess_input(image)
+
+    if label is None:
+        return image
+    else:
+        return image, label
+
+
+def make_tf_dataset(images, labels, is_train):
+    images = tf.data.Dataset.from_tensor_slices(images)
+    images = images.map(preprocess_image, num_parallel_calls=AUTO)
+    labels = tf.data.Dataset.from_tensor_slices(labels)
+
+    dataset = tf.data.Dataset.zip((images, labels))
+    dataset = dataset.repeat()
 
     if is_train:
-        dataset = (tf.data.Dataset
-                   .from_tensor_slices((images, labels))
-                   .map(preprocess_image, num_parallel_calls=AUTO)
-                   .repeat()
-                   .shuffle(512)
-                   .batch(BATCH_SIZE)
-                   .prefetch(AUTO)
-        )
-    
+        dataset = dataset.map(partial(process_data, img_size=IMG_SIZE), num_parallel_calls=AUTO)
+        dataset = dataset.batch(BATCH_SIZE)
+        dataset = dataset.prefetch(AUTO)
+
     else:
-        dataset = (tf.data.Dataset
-                   .from_tensor_slices((images, labels))
-                   .map(preprocess_image, num_parallel_calls=AUTO)
-                   .repeat()
-                   .batch(BATCH_SIZE)
-                   .prefetch(AUTO)
-        )
+        dataset = dataset.batch(BATCH_SIZE)
+        dataset = dataset.prefetch(AUTO)
 
-    return dataset, total_images, num_labels
-
-
-def tf_data_visualize(dataset):
-    for image, label in dataset.take(1):
-        print("Image shape: ", image.numpy().shape)
-        print("Label: ", label.numpy().shape)
-
-    image_batch, label_batch = next(iter(dataset))
-
-    plt.figure(figsize=(10, 10))
-    for i in range(16):
-        ax = plt.subplot(4, 4, i + 1)
-        plt.imshow((image_batch[i].numpy()).astype('uint8'))
-        label = label_batch[i]
-        plt.axis('off')
-
-    plt.show()
+    return dataset
 
 
 def build_lrfn(lr_start=0.00001, lr_max=0.00005, 
@@ -119,6 +122,55 @@ def build_lrfn(lr_start=0.00001, lr_max=0.00005,
                                 - lr_sustain_epochs) + lr_min
         return lr
     return lrfn
+
+
+def tf_data_visualize(augmentation_element):
+    row, col, idx = 5, 4, 0
+    row = min(row, BATCH_SIZE // col)
+
+    for (image, label) in augmentation_element:
+        image = image / 255.0
+        plt.figure(figsize=(15, int(15 * row / col)))
+        for j in range(row * col):
+            plt.subplot(row, col, j + 1)
+            plt.axis('off')
+            plt.imshow(image[j, ])
+
+        plt.savefig(f'{SAVED_PATH}/{LOG_TIME}/result_{idx}.jpg')
+        plt.show()
+        idx += 1
+
+        if idx == 3:
+            break
+
+
+def str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+
+def get_model():
+    with strategy.scope():
+        base_model = tf.keras.applications.EfficientNetB1(input_shape=(IMG_SIZE, IMG_SIZE, 3),
+                                    weights="imagenet", # noisy-student
+                                    include_top=False)
+        for layer in base_model.layers:
+            layer.trainable = True
+
+        avg = tf.keras.layers.GlobalAveragePooling2D()(base_model.output)
+        batch_norm = tf.keras.layers.BatchNormalization()(avg)
+        drop_out = tf.keras.layers.Dropout(0.2)(batch_norm)
+        output = tf.keras.layers.Dense(num_classes, activation="softmax")(drop_out)
+        
+        model = tf.keras.Model(inputs=base_model.input, outputs=output)
+
+    model.compile(optimizer='adam', loss = 'categorical_crossentropy', metrics = ['categorical_accuracy'])
+    model.summary()
+    return model
 
 
 def display_training_curves(history):
@@ -147,35 +199,6 @@ def display_training_curves(history):
     plt.show()
 
 
-def str2bool(v):
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
-        return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
-        return False
-    else:
-        raise argparse.ArgumentTypeError('Boolean value expected.')
-
-
-def get_model():
-    with strategy.scope():
-        base_model = tf.keras.applications.EfficientNetB1(input_shape=(IMG_SIZE, IMG_SIZE, 3),
-                                    weights="imagenet", # noisy-student
-                                    include_top=False)
-        for layer in base_model.layers:
-            layer.trainable = True
-
-        avg = tf.keras.layers.GlobalAveragePooling2D()(base_model.output)
-        batch_norm = tf.keras.layers.BatchNormalization()(avg)
-        drop_out = tf.keras.layers.Dropout(0.2)(batch_norm)
-        output = tf.keras.layers.Dense(train_labels, activation="softmax")(drop_out)
-        
-        model = tf.keras.Model(inputs=base_model.input, outputs=output)
-
-    model.compile(optimizer='adam', loss = 'categorical_crossentropy', metrics = ['categorical_accuracy'])
-    model.summary()
-    return model
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Image classification model Training")
     parser.add_argument('--input_dataset', type=str)
@@ -183,21 +206,13 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     dataset = args.input_dataset
-    DATASET_NAME = dataset.split('/')[-3]
-    DATE = dataset.split('/')[-2]
-    TRAIN_PATH = f'/data/backup/pervinco_2020/Auged_datasets/{DATASET_NAME}/{DATE}/train'
-    VALID_PATH = f'/data/backup/pervinco_2020/Auged_datasets/{DATASET_NAME}/{DATE}/valid'
+    DATASET_NAME = dataset.split('/')[-1]
 
-    train_dataset, train_total, train_labels = make_tf_dataset(TRAIN_PATH, True)
-    valid_dataset, valid_total, valid_labels = make_tf_dataset(VALID_PATH, False)
+    total_images, total_labels, num_classes = basic_processing(dataset)
+    train_images, valid_images, train_labels, valid_labels = train_test_split(total_images, total_labels, test_size=.3, random_state=777)
+    train_dataset = make_tf_dataset(train_images, train_labels, True)
+    valid_dataset = make_tf_dataset(valid_images, valid_labels, False)
 
-    print(train_labels, valid_labels)
-
-    if args.visualize == True:
-        for i in range(3):
-            tf_data_visualize(train_dataset)
-            tf_data_visualize(valid_dataset)
-    
     # Learning Rate Scheduler setup
     lrfn = build_lrfn()
     lr_schedule = tf.keras.callbacks.LearningRateScheduler(lrfn, verbose=1)
@@ -211,12 +226,17 @@ if __name__ == "__main__":
     if not(os.path.isdir(f'/{SAVED_PATH}/{LOG_TIME}')):
         os.makedirs(f'/{SAVED_PATH}/{LOG_TIME}')
 
+    print(train_labels, valid_labels)
+    if args.visualize:
+        tf_data_visualize(train_dataset)
+
     checkpointer = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
                                                       monitor='val_categorical_accuracy',
                                                       save_best_only=True,
                                                       mode='max')
     earlystopper = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=3)
 
+    train_total, valid_total = len(train_images), len(valid_images)
     TRAIN_STEPS_PER_EPOCH = int(tf.math.ceil(train_total/ BATCH_SIZE).numpy())
     VALID_STEP_PER_EPOCH = int(tf.math.ceil(valid_total / BATCH_SIZE).numpy())
 
