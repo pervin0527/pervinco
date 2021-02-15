43ebedc20a5e637dd58fbc7fbaacbdeb0f4a8d97
pervin0527 <zfbtldnz@gmail.com>
2021-02-15 11:36:54 +0900

    updated 2021.02.15

--- source/EMNIST/denoising.py
+++ source/EMNIST/denoising.py
@@ -3,19 +3,20 @@ import numpy as np
 np.set_printoptions(threshold=sys.maxsize)
 from matplotlib import pyplot as plt
 import albumentations as A
+import tensorflow as tf
 
 transforms = A.Compose([
                 # A.Resize(10, 10, p=1),
-                # A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225), p=1),
-                A.MedianBlur(blur_limit=3, always_apply=True, p=0.4),
-                A.HorizontalFlip(p=0.4),
-                A.VerticalFlip(p=0.4),
-                A.RandomRotate90(p=0.5),
+                A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225), p=1),
+                # A.MedianBlur(blur_limit=3, always_apply=True, p=0.4),
+                # A.HorizontalFlip(p=0.4),
+                # A.VerticalFlip(p=0.4),
+                # A.RandomRotate90(p=0.5),
             ])
 
 
 """salt pepper noise filter"""
-image_path = '/data/backup/pervinco/datasets/dirty_mnist_2/dirty_mnist_2nd/00000.png'
+image_path = '/data/backup/pervinco/datasets/dirty_mnist_2/dirty_mnist_2nd/00018.png'
 
 image = cv2.imread(image_path) # cv2.IMREAD_GRAYSCALE
 cv2.imshow('original', image)
@@ -34,3 +35,9 @@ image5 = image4 - image2
 cv2.imshow('sub', image5)
 
 cv2.waitKey(0)
+
+alb_test = transforms(image=image5)['image']
+print(alb_test[0][:5])
+
+tf_test = tf.keras.applications.resnet.preprocess_input(image5)
+print(tf_test[0][:5])
