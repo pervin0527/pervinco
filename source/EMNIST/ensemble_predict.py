48668c2a22c6b7bf5a359fc5bae14531281dd048
pervin0527 <zfbtldnz@gmail.com>
2021-02-24 17:17:14 +0900

    updated 2021.02.24

--- source/EMNIST/ensemble_predict.py
+++ source/EMNIST/ensemble_predict.py
@@ -2,7 +2,6 @@ import cv2, datetime, glob, string, os
 import pandas as pd
 import numpy as np
 import tensorflow as tf
-import albumentations as A
 
 # GPU setup
 gpus = tf.config.experimental.list_physical_devices('GPU')
@@ -29,8 +28,7 @@ def get_images(test_files):
     for path in test_files:
         image = cv2.imread(f'{test_image_path}/{path}.png')
         image2 = np.where((image <= 254) & (image != 0), 0, image)
-        image2 = cv2.resize(image2, (330, 330))
-        image2 = test_transforms(image=image2)['image']
+        image2 = cv2.resize(image2, (RE_SIZE, RE_SIZE))
         image2 = tf.keras.applications.resnet.preprocess_input(image2)
         images.append(image2)
 
@@ -51,30 +49,23 @@ def ensemble(test_images):
 
     print(f"total shape : {preds.shape}")
     preds = preds / len(models)
-    preds = (preds > 0.6) * 1
+    preds = (preds > 0.5) * 1
 
     return preds
         
 
 if __name__ == "__main__":
     IMG_SIZE = 256
+    RE_SIZE = 380
     LOG_TIME = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
-    BASE_PATH = '/data/backup/pervinco/datasets/dirty_mnist_2'
-    MODEL_PATH = '/data/backup/pervinco/model/dirty_mnist/merge3_85_330_10'
+    BASE_PATH = '/data/tf_workspace/datasets/dirty_mnist_2'
+    MODEL_PATH = '/data/tf_workspace/model/dirty_mnist/2021_02_24_10_50'
     
     test_image_path = f'{BASE_PATH}/test_dirty_mnist_2nd'
     sample_submission = f'{BASE_PATH}/sample_submission.csv'
     submission_df = pd.read_csv(sample_submission)
     
     CLASSES = list(string.ascii_lowercase)
-
-    test_transforms = A.Compose([
-        A.Resize(330, 330, p=1),
-        A.OneOf([
-            A.HorizontalFlip(p=0.8),
-            A.VerticalFlip(p=0.8),
-            A.RandomRotate90(p=0.9),], p=1),
-    ])
 
     test_images = submission_df['index']
     test_images = get_images(test_images)
