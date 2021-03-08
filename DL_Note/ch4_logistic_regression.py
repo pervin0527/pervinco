85f9aeec84c645bb1a86b3232aa3ac14c88ea032
pervin0527 <zfbtldnz@gmail.com>
2021-03-08 14:07:44 +0900

    updated 2021.03.08

--- DL_Note/ch4_logistic_regression.py
+++ DL_Note/ch4_logistic_regression.py
@@ -17,39 +17,47 @@ y_train = [[0.],
 x_test = [[5.,2.]]
 y_test = [[1.]]
 
-dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train)).batch(len(x_train)) #.repeat()
+dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train)).batch(len(x_train)) #tf.data API형태로 적용
 
+# 값이 모두 0인 2행 1열 Weight, 값이 0인 상수 bias
 W = tf.Variable(tf.zeros([2, 1]), name='weight')
 b = tf.Variable(tf.zeros([1]), name='bias')
+print(W, b)
 
+# Logistic Regression에 맞는 hypothesis를 정의
 def logistic_regression(features):
     hypothesis = tf.divide(1., 1. + tf.exp(tf.matmul(features, W) + b))
+
     return hypothesis
+
 
 def loss_fn(hypothesis, features, labels):
     cost = -tf.reduce_mean(labels * tf.math.log(logistic_regression(features)) + (1 - labels) * tf.math.log(1 - hypothesis))
+    
     return cost
 
 optimizer = tf.keras.optimizers.SGD(learning_rate=0.01)
 
 def accuracy_fn(hypothesis, labels):
     predicted = tf.cast(hypothesis > 0.5, dtype=tf.float32)
-    accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted, labels), dtype=tf.int32))
+    print(predicted) # 출력 값이 tf.Tensor([[1.]], shape=(1, 1), dtype=float32)이므로, 1에 해당하는 것을 알 수 있다.
+    accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted, labels), dtype=tf.int32)) # y_test와 같음을 평가하는 accuracy 출력.
+    
     return accuracy
 
 def grad(features, labels):
     with tf.GradientTape() as tape:
-        loss_value = loss_fn(logistic_regression(features),features,labels)
-    return tape.gradient(loss_value, [W,b])
+        loss_value = loss_fn(logistic_regression(features), features, labels)
+    return tape.gradient(loss_value, [W, b])
 
 EPOCHS = 1001
 
 for step in range(EPOCHS):
     for features, labels  in iter(dataset):
         grads = grad(features, labels)
-        optimizer.apply_gradients(grads_and_vars=zip(grads,[W,b]))
+        optimizer.apply_gradients(grads_and_vars=zip(grads, [W, b]))
         if step % 100 == 0:
-            print("Iter: {}, Loss: {:.4f}".format(step, loss_fn(logistic_regression(features),features,labels)))
+            print("Iter: {}, Loss: {:.4f}".format(step, loss_fn(logistic_regression(features), features, labels)))
 
-test_acc = accuracy_fn(logistic_regression(x_test),y_test)
+test_acc = accuracy_fn(logistic_regression(x_test), y_test)
 print("Testset Accuracy: {:.4f}".format(test_acc))
