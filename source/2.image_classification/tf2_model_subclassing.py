16a17afe85958d6b790e5d7a2296d40c745af1d5
pervin0527 <zfbtldnz@gmail.com>
2021-03-27 17:02:22 +0900

    updated 2021.03.27

---
+++ source/2.image_classification/tf2_model_subclassing.py
@@ -0,0 +1,134 @@
+import tensorflow as tf
+import numpy as np
+import os
+
+# hyper parameters
+learning_rate = 0.001
+training_epochs = 15
+batch_size = 100
+
+tf.random.set_seed(777)
+
+# checkpoint directory
+cur_dir = os.getcwd()
+ckpt_dir_name = 'checkpoints'
+model_dir_name = 'minst_cnn_subclass'
+
+checkpoint_dir = os.path.join(cur_dir, ckpt_dir_name, model_dir_name)
+os.makedirs(checkpoint_dir, exist_ok=True)
+
+checkpoint_prefix = os.path.join(checkpoint_dir, model_dir_name)
+
+# load mnist
+mnist = tf.keras.datasets.mnist
+class_names = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+# datasets
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()    
+    
+train_images = train_images.astype(np.float32) / 255.
+test_images = test_images.astype(np.float32) / 255.
+train_images = np.expand_dims(train_images, axis=-1)
+test_images = np.expand_dims(test_images, axis=-1)
+    
+train_labels = tf.keras.utils.to_categorical(train_labels, 10)
+test_labels = tf.keras.utils.to_categorical(test_labels, 10)    
+    
+train_dataset = tf.data.Dataset.from_tensor_slices((train_images, train_labels)).shuffle(
+                buffer_size=100000).batch(batch_size)
+test_dataset = tf.data.Dataset.from_tensor_slices((test_images, test_labels)).batch(batch_size)
+
+# model subclassing
+class MNISTModel(tf.keras.Model):
+    def __init__(self):
+        super(MNISTModel, self).__init__()
+        self.conv1 = tf.keras.layers.Conv2D(filters=32, kernel_size=[3, 3], padding='SAME', activation=tf.nn.relu)
+        self.pool1 = tf.keras.layers.MaxPool2D(padding='SAME')
+        self.conv2 = tf.keras.layers.Conv2D(filters=64, kernel_size=[3, 3], padding='SAME', activation=tf.nn.relu)
+        self.pool2 = tf.keras.layers.MaxPool2D(padding='SAME')
+        self.conv3 = tf.keras.layers.Conv2D(filters=128, kernel_size=[3, 3], padding='SAME', activation=tf.nn.relu)
+        self.pool3 = tf.keras.layers.MaxPool2D(padding='SAME')
+        self.pool3_flat = tf.keras.layers.Flatten()
+        self.dense4 = tf.keras.layers.Dense(units=256, activation=tf.nn.relu)
+        self.drop4 = tf.keras.layers.Dropout(rate=0.4)
+        self.dense5 = tf.keras.layers.Dense(units=10)
+
+    def call(selfm inputs, trainig=False):
+        net = self.conv1(inputs)
+        net = self.pool1(net)
+        net = self.conv2(net)
+        net = self.pool2(net)
+        net = self.conv3(net)
+        net = self.pool3(net)
+        net = self.pool3_flat(net)
+        net = self.dense4(net)
+        net = self.drop4(net)
+        net = self.dense5(net)
+        return net
+
+model = MNISTModel()
+temp_inputs = tf.keras.Input(shape=(28, 28, 1))
+model(temp_inputs)
+model.summary()
+
+@tf.function
+def loss_fn(model, images, labels):
+    logits = model(images, training=True)
+    loss = tf.reduce_mean(tf.keras.losses.categorical_crossentropy(y_pred=logits, y_true=labels, from_logits=True))     
+    return loss
+
+@tf.function
+def grad(model, images, labels):
+    with tf.GradientTape() as tape:
+        loss = loss_fn(model, images, labels)
+    return tape.gradient(loss, model.variables)
+
+@tf.function
+def evaluate(model, images, labels):
+    logits = model(images, training=False)
+    correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(labels, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    return accuracy
+
+optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+checkpoint = tf.train.Checkpoint(cnn=model)
+
+@tf.function
+def train(model, images, labels):
+    grads = grad(model, images, labels)
+    optimizer.apply_gradients(zip(grads, model.trainable_variables))
+
+# train my model
+print('Learning started. It takes sometime.')
+for epoch in range(training_epochs):
+    avg_loss = 0.
+    avg_train_acc = 0.
+    avg_test_acc = 0.
+    train_step = 0
+    test_step = 0
+    
+    for images, labels in train_dataset:
+        train(model, images, labels)
+        #grads = grad(model, images, labels)                
+        #optimizer.apply_gradients(zip(grads, model.variables))
+        loss = loss_fn(model, images, labels)
+        acc = evaluate(model, images, labels)
+        avg_loss = avg_loss + loss
+        avg_train_acc = avg_train_acc + acc
+        train_step += 1
+    avg_loss = avg_loss / train_step
+    avg_train_acc = avg_train_acc / train_step
+    
+    for images, labels in test_dataset:        
+        acc = evaluate(model, images, labels)        
+        avg_test_acc = avg_test_acc + acc
+        test_step += 1    
+    avg_test_acc = avg_test_acc / test_step    
+
+    print('Epoch:', '{}'.format(epoch + 1), 'loss =', '{:.8f}'.format(avg_loss), 
+          'train accuracy = ', '{:.4f}'.format(avg_train_acc), 
+          'test accuracy = ', '{:.4f}'.format(avg_test_acc))
+    
+    checkpoint.save(file_prefix=checkpoint_prefix)
+
+print('Learning Finished!')
