95315b340f7bfffb2ee740e8c4c9f13e6661074c
pervin0527 <zfbtldnz@gmail.com>
2021-06-07 18:04:15 +0900

    updated 2021.06.07

--- source/3.object_detection/yolov4/oid_to_xml.py
+++ source/3.object_detection/yolov4/oid_to_xml.py
@@ -1,168 +1,161 @@
 import os
-from tqdm import tqdm
-from sys import exit
-import argparse
 import cv2
-from textwrap import dedent
+import glob
+import pathlib
+import argparse
+import time
+import datetime
+from tqdm import tqdm
 from lxml import etree
 
-XML_DIR = ''
+def write_xml():
+    DIRS = sorted(glob.glob(f'{dataset_path}/*'))
+    
+    for INDEX, DIR in enumerate(DIRS):
+        if " " in DIR:
+            os.rename(DIR, DIR.replace(" ", "_"))
 
-#os.chdir('Dataset')
-os.chdir(os.path.join("OID", "Dataset"))
-DIRS = os.listdir(os.getcwd())
+        print("\n" + "Creating PASCAL VOC XML Files for Class:", DIR)
 
-for DIR in DIRS:
-    if os.path.isdir(DIR):
-        os.chdir(DIR)
+        f_path = pathlib.Path(DIR)
 
-        print("Currently in Subdirectory:", DIR)
-        CLASS_DIRS = os.listdir(os.getcwd()) 
-        for CLASS_DIR in CLASS_DIRS:
-            if " " in CLASS_DIR:
-                os.rename(CLASS_DIR, CLASS_DIR.replace(" ", "_"))
-        
-        CLASS_DIRS = os.listdir(os.getcwd())
-        for CLASS_DIR in CLASS_DIRS:
-            #if " " in CLASS_DIR:
-            #    os.rename(CLASS_DIR, CLASS_DIR.replace(" ", "_"))
-            if os.path.isdir(CLASS_DIR):
-                os.chdir(CLASS_DIR)
+        images = list(f_path.glob('*.jpg'))
+        images = [str(path) for path in images]
 
-                print("\n" + "Creating PASCAL VOC XML Files for Class:", CLASS_DIR)
-                # Create Directory for annotations if it does not exist yet
-                #if not os.path.exists(XML_DIR):
-                #    os.makedirs(XML_DIR)
+        # labels = list(f_path.glob('Label/*.txt'))
+        # labels = [str(path) for path in labels]
 
-                #Read Labels from OIDv4 ToolKit
-                os.chdir("Label")
+        label = DIR.split('/')[-1]
 
-                #Create PASCAL XML
-                for filename in tqdm(os.listdir(os.getcwd())):
-                    if filename.endswith(".txt"):
-                        filename_str = str.split(filename, ".")[0]
+        if label == 'Computer_monitor':
+            label = 'Television'
+
+        elif label == "Person" or label == 'Woman':
+            label = 'Man'
+
+        elif label == 'Desk':
+            label = 'Table'
+
+        for idx in tqdm(range(len(images))):
+            # sub = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+
+            filename = images[idx]
+            filename_str = (filename.split('/')[-1]).split('.')[0]
+
+            image = cv2.imread(filename)
+            cv2.imwrite(f'{output_path}/{label}_{INDEX}_{idx}.jpg', image)       
+
+            annotation = etree.Element("annotation")
+
+            folder = etree.Element('folder')
+            folder.text = filename.split('/')[-2]
+            annotation.append(folder)
+
+            filename_xml = etree.Element("filename")
+            filename_xml.text = f'{label}_{INDEX}_{idx}.jpg'
+            annotation.append(filename_xml)
+
+            path = etree.Element("path")
+            path.text = '/'.join(filename.split('/')[:-1]) + f'/{filename_xml.text}'
+            annotation.append(path)
+
+            source = etree.Element("source")
+            annotation.append(source)
+
+            database = etree.Element("database")
+            database.text = "Unknown"
+            source.append(database)
+
+            size = etree.Element("size")
+            annotation.append(size)
+
+            width = etree.Element("width")
+            height = etree.Element("height")
+            depth = etree.Element("depth")
+
+            width.text = str(image.shape[1])
+            height.text = str(image.shape[0])
+            depth.text = str(image.shape[2])
+
+            size.append(width)
+            size.append(height)
+            size.append(depth)
+
+            label_original = open(f'{"/".join(filename.split("/")[:-1])}/Label/{filename_str}.txt', 'r')
+
+            for line in label_original:
+                line = line.strip()
+                l = line.split(' ')
+                class_name = l[0]
+                try:
+                    xmin_l = str(int(float(l[1])))
+                    add1 = 0
+                except ValueError:
+                    class_name = l[0]+"_"+l[1]
+                    add1 = 1
+
+                xmin_l = str(int(float(l[1+add1])))
+                ymin_l = str(int(float(l[2+add1])))
+                xmax_l = str(int(float(l[3+add1])))
+                ymax_l = str(int(float(l[4+add1])))
+                
+                obj = etree.Element("object")
+                annotation.append(obj)
+
+                name = etree.Element("name")
+                name.text = class_name
+                obj.append(name)
+
+                pose = etree.Element("pose")
+                pose.text = "Unspecified"
+                obj.append(pose)
+
+                truncated = etree.Element("truncated")
+                truncated.text = "0"
+                obj.append(truncated)
+
+                difficult = etree.Element("difficult")
+                difficult.text = "0"
+                obj.append(difficult)
+
+                bndbox = etree.Element("bndbox")
+                obj.append(bndbox)
+
+                xmin = etree.Element("xmin")
+                xmin.text = xmin_l
+                bndbox.append(xmin)
+
+                ymin = etree.Element("ymin")
+                ymin.text = ymin_l
+                bndbox.append(ymin)
+
+                xmax = etree.Element("xmax")
+                xmax.text = xmax_l
+                bndbox.append(xmax)
+
+                ymax = etree.Element("ymax")
+                ymax.text = ymax_l
+                bndbox.append(ymax)
+
+            s = etree.tostring(annotation, pretty_print=True)
+            with open(f'{output_path}/{label}_{INDEX}_{idx}.xml', 'wb') as f:
+                f.write(s)
+                f.close()
+
+            # break
 
 
-                        annotation = etree.Element("annotation")
-                        
-                        os.chdir("..")
-                        folder = etree.Element("folder")
-                        folder.text = os.path.basename(os.getcwd())
-                        annotation.append(folder)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="OID xml maker")
+    parser.add_argument('--input_ds', type=str)
+    parser.add_argument('--output_path', type=str)
+    args = parser.parse_args()
 
-                        filename_xml = etree.Element("filename")
-                        filename_xml.text = filename_str + ".jpg"
-                        annotation.append(filename_xml)
+    dataset_path = args.input_ds
+    output_path = args.output_path
+    print(dataset_path)
 
-                        path = etree.Element("path")
-                        path.text = os.path.join(os.path.dirname(os.path.abspath(filename)), filename_str + ".jpg")
-                        annotation.append(path)
+    if not os.path.isdir(output_path):
+        os.makedirs(output_path)
 
-                        source = etree.Element("source")
-                        annotation.append(source)
-
-                        database = etree.Element("database")
-                        database.text = "Unknown"
-                        source.append(database)
-
-                        size = etree.Element("size")
-                        annotation.append(size)
-
-                        width = etree.Element("width")
-                        height = etree.Element("height")
-                        depth = etree.Element("depth")
-
-                        img = cv2.imread(filename_xml.text)
-
-                        try:
-                            width.text = str(img.shape[1])
-                        except AttributeError:
-                            #os.chdir("..")
-                            os.chdir("Label")
-                            continue
-                        height.text = str(img.shape[0])
-                        depth.text = str(img.shape[2])
-
-                        size.append(width)
-                        size.append(height)
-                        size.append(depth)
-
-                        segmented = etree.Element("segmented")
-                        segmented.text = "0"
-                        annotation.append(segmented)
-
-                        os.chdir("Label")
-                        label_original = open(filename, 'r')
-
-                        # Labels from OIDv4 Toolkit: name_of_class X_min Y_min X_max Y_max
-                        for line in label_original:
-                            line = line.strip()
-                            l = line.split(' ')
-                            class_name = l[0]
-                            try:
-                                xmin_l = str(int(float(l[1])))
-                                add1 = 0
-                            except ValueError:
-                                class_name = l[0]+"_"+l[1]
-                                add1 = 1
-
-                            xmin_l = str(int(float(l[1+add1])))
-                            ymin_l = str(int(float(l[2+add1])))
-                            xmax_l = str(int(float(l[3+add1])))
-                            ymax_l = str(int(float(l[4+add1])))
-                            
-                            obj = etree.Element("object")
-                            annotation.append(obj)
-
-                            name = etree.Element("name")
-                            name.text = class_name
-                            obj.append(name)
-
-                            pose = etree.Element("pose")
-                            pose.text = "Unspecified"
-                            obj.append(pose)
-
-                            truncated = etree.Element("truncated")
-                            truncated.text = "0"
-                            obj.append(truncated)
-
-                            difficult = etree.Element("difficult")
-                            difficult.text = "0"
-                            obj.append(difficult)
-
-                            bndbox = etree.Element("bndbox")
-                            obj.append(bndbox)
-
-                            xmin = etree.Element("xmin")
-                            xmin.text = xmin_l
-                            bndbox.append(xmin)
-
-                            ymin = etree.Element("ymin")
-                            ymin.text = ymin_l
-                            bndbox.append(ymin)
-
-                            xmax = etree.Element("xmax")
-                            xmax.text = xmax_l
-                            bndbox.append(xmax)
-
-                            ymax = etree.Element("ymax")
-                            ymax.text = ymax_l
-                            bndbox.append(ymax)
-
-                        os.chdir("..")
-
-                        #os.chdir(XML_DIR)
-
-                        # write xml to file
-                        s = etree.tostring(annotation, pretty_print=True)
-                        with open(filename_str + ".xml", 'wb') as f:
-                            f.write(s)
-                            f.close()
-
-                        #os.chdir("..")
-                        os.chdir("Label")
-
-                os.chdir("..")
-                os.chdir("..")   
-                   
-        os.chdir("..")
+    write_xml()
