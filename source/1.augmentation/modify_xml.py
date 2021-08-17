c956f0c5823288c0d6e974fef765d742bd9a1ff6
pervin0527 <zfbtldnz@gmail.com>
2021-08-17 16:38:51 +0900

    updated 2021.08.17

--- source/1.augmentation/modify_xml.py
+++ source/1.augmentation/modify_xml.py
@@ -1,15 +1,16 @@
 import cv2
 import pathlib
 import xml.etree.ElementTree as ET
+from lxml.etree import Element, SubElement, tostring
 
 from xml.dom import minidom
 
 def modify_xml(images, xmls, is_train):
     if is_train:
-        output_path = f'{PATH}/train2'
+        output_path = f'{PATH}/train'
 
     else:
-        output_path = f'{PATH}/test2'
+        output_path = f'{PATH}/test'
 
     for xml in xmls:
         file_name = xml.split('/')[-1]
@@ -17,19 +18,32 @@ def modify_xml(images, xmls, is_train):
         root = tree.getroot()
         obj_xml = root.findall('annotation')
 
-        img_file_name = root.find('filename').text
-        root.find('filename').text = img_file_name.split('.')[0] + '.jpg'
-        tree.write(f'{output_path}/{file_name}')
+        if is_train:
+            folder_name = 'train'
 
-    for image in images:
-        img_name = image.split('/')[-1]
-        img_name = img_name.split('.')[0]
-        img = cv2.imread(image)
-        cv2.imwrite(f'{output_path}/{img_name}.jpg', img)
+        else:
+            folder_name = 'test'
+
+        root.find('folder').text = folder_name
+
+        try:
+            img_file_name = root.find('filename').text
+            print(img_file_name)
+            root.find('filename').text = img_file_name.split('.')[0] + '.jpg'
+            tree.write(f'{output_path}/{file_name}')
+
+        # for image in images:
+        #     img_name = image.split('/')[-1]
+        #     img_name = img_name.split('.')[0]
+        #     img = cv2.imread(image)
+        #     cv2.imwrite(f'{output_path}/{img_name}.jpg', img)
+
+        except:
+            pass
 
 
 def get_lists(ds_path):
-    images = list(ds_path.glob('*.png'))
+    images = list(ds_path.glob('*.jpg'))
     images = [str(path) for path in images]
 
     xmls = list(ds_path.glob('*.xml'))
@@ -39,7 +53,7 @@ def get_lists(ds_path):
 
 
 if __name__ == "__main__":
-    PATH = "/data/Datasets/Seeds/mm_data"
+    PATH = "/data/Datasets/Seeds/ETRI_detection/augmentations"
 
     train_path = pathlib.Path(f'{PATH}/train')
     test_path = pathlib.Path(f'{PATH}/test')
