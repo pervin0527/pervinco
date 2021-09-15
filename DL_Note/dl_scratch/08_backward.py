ad8064bd02f828b334a57b2b0799ad292fd0cc81
pervin0527 <zfbtldnz@gmail.com>
2021-09-15 09:29:52 +0900

    updated 2021.09.15

---
+++ DL_Note/dl_scratch/08_backward.py
@@ -0,0 +1,60 @@
+# 덧셈 계층
+class AddLayer:
+    def __init__(self):
+        pass
+
+    def forward(self, x, y):
+        out = x + y
+        return out
+
+    def backward(self, dout):
+        dx = dout * 1
+        dy = dout * 1
+        return dx, dy
+
+# 곱셈 계층
+class MulLayer:
+    def __init__(self):
+        self.x = None
+        self.y = None
+
+    def forward(self, x, y):
+        self.x = x
+        self.y = y
+        out = x * y
+
+        return out
+
+    def backward(self, dout):
+        # dout은 상류애서 넘어온 미분값.
+        dx = dout * self.y
+        dy = dout * self.x
+
+        return dx, dy
+
+apple = 100
+apple_num = 2
+orange = 150
+orange_num = 3
+tax = 1.1
+
+mul_apple_layer = MulLayer()
+mul_orange_layer = MulLayer()
+add_apple_orange_layer = AddLayer()
+mul_tax_layer = MulLayer()
+
+# 순전파
+apple_price = mul_apple_layer.forward(apple, apple_num)
+orange_price = mul_orange_layer.forward(orange, orange_num)
+all_price = add_apple_orange_layer.forward(apple_price, orange_price)
+price = mul_tax_layer.forward(all_price, tax)
+print(price)
+
+# 역전파
+dprice = 1 # 순전파 출력에 대한 미분 값.
+dall_price, dtax = mul_tax_layer.backward(dprice)
+dapple_price, dorange_price = add_apple_orange_layer.backward(dall_price)
+dorange, dorange_num = mul_orange_layer.backward(dorange_price)
+dapple, dapple_num = mul_apple_layer.backward(dapple_price)
+
+print(dapple_num, dapple, dorange, dorange_num, dtax)
