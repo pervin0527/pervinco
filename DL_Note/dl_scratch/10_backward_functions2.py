eab932fa81199eae2e8b861ff5d44faa8e57b049
pervin0527 <zfbtldnz@gmail.com>
2021-09-25 22:28:09 +0900

    updated 2021.09.25

---
+++ DL_Note/dl_scratch/10_backward_functions2.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+class Affine:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        
+        self.x = None
+        self.original_x_shape = None
+        # 가중치와 편향 매개변수의 미분
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        # 텐서 대응
+        self.original_x_shape = x.shape
+        x = x.reshape(x.shape[0], -1)
+        self.x = x
+
+        out = np.dot(self.x, self.W) + self.b
+
+        return out
+
+    def backward(self, dout):
+        dx = np.dot(dout, self.W.T)
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+        
+        dx = dx.reshape(*self.original_x_shape)  # 입력 데이터 모양 변경(텐서 대응)
+        return dx
+
+X = np.random.rand(2, 2)
+W = np.random.rand(2, 3)
+B = np.random.rand(3)
+
+test = Affine(W, B)
+fw = test.forward(X)
+print(fw)
+bw = test.backward(fw)
+print(bw)
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None
+        self.y = None
+        self.t = None
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
+
+        return dx
