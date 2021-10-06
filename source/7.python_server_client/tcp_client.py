018b45c1c9a4e8794caa6968b8a5611551687be4
pervin0527 <zfbtldnz@gmail.com>
2021-10-06 10:39:44 +0900

    updated 2021.10.06

--- source/7.python_server_client/tcp_client.py
+++ source/7.python_server_client/tcp_client.py
@@ -1,14 +1,21 @@
+import cv2
 import socket
+import numpy as np
 
-HOST = '221.148.121.77'  # 서버의 주소. hostname 또는 ip address를 사용할 수 있다.
-PORT = 1025 # 서버에서 지정한 포트 번호. 
+HOST = 'localhost'
+PORT = 8888
   
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # 소켓 객체를 생성. 주소 체계(address family)로 IPv4, 소켓 타입으로 TCP 사용.
-client_socket.connect((HOST, PORT)) # 지정한 HOST와 PORT를 사용하여 서버에 접속. 
-client_socket.sendall('안녕'.encode()) # 메시지를 전송
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((HOST, PORT))
 
-data = client_socket.recv(1024) # 메시지를 수신. 
-print('Received', repr(data.decode()))
+image_path = "/data/Datasets/testset/ETRI_cropped_large/test_sample_04.jpg"
+image = cv2.imread(image_path)
+image = cv2.resize(image, (416, 416))
+encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+result, imgencode = cv2.imencode('.jpg', image, encode_param)
+data = np.array(imgencode)
+stringData = data.tobytes()
 
-# 소켓을 닫음.
+client_socket.send(str(len(stringData)).ljust(16).encode())
+client_socket.send(stringData)
 client_socket.close()
--- source/7.python_server_client/tcp_server.py
+++ source/7.python_server_client/tcp_server.py
@@ -1,31 +1,35 @@
+import cv2
 import socket
+import numpy as np
 
-HOST = '221.148.121.77' # 접속할 서버 주소. 여기에서는 루프백(loopback) 인터페이스 주소 즉 localhost를 사용.    
-PORT = 1025 # 클라이언트 접속을 대기하는 포트 번호.
+def recvall(sock, count):
+    buf = b''
+
+    while count:
+        newbuf = sock.recv(count)
+        if not newbuf:
+            return None
+
+        buf += newbuf
+        count -= len(newbuf)
+
+    return buf
+
+HOST = 'localhost'
+PORT = 8888
   
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # 소켓 객체를 생성. 주소 체계(address family)로 IPv4, 소켓 타입으로 TCP 사용. 
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # 포트 사용중이라 연결할 수 없다는 WinError 10048 에러 해결를 위해 필요.
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((HOST, PORT))
+server_socket.listen(True)
 
+connect, address = server_socket.accept()
 
-# bind 함수는 소켓을 특정 네트워크 인터페이스와 포트 번호에 연결하는데 사용.
-# HOST는 hostname, ip address, 빈 문자열 ""이 될 수 있고 빈 문자열이면 모든 네트워크 인터페이스로부터의 접속을 허용
-# PORT는 1-65535 사이의 숫자를 사용할 수 있다.  
-server_socket.bind((HOST, PORT)) 
-server_socket.listen() # 서버가 클라이언트의 접속을 허용하도록 합니다. 
-client_socket, addr = server_socket.accept() # accept 함수에서 대기하다가 클라이언트가 접속하면 새로운 소켓을 리턴.
-print('Connected by', addr) # 접속한 클라이언트의 주소입니다.
+length = recvall(connect, 16)
+stringData = recvall(connect, int(length))
+data = np.frombuffer(stringData, dtype='uint8')
 
-while True:
+data = cv2.imdecode(data, 1)
+cv2.imshow('test', data)
+cv2.waitKey(0)
 
-    # 클라이언트가 보낸 메시지를 수신하기 위해 대기.
-    data = client_socket.recv(1024)
- 
-    if not data: # 빈 문자열을 수신하면 루프를 중지.
-        break
-
-    print('Received from', addr, data.decode()) # 수신받은 문자열을 출력. 
-    client_socket.sendall(data) # 받은 문자열을 다시 클라이언트로 전송해줍니다.(에코)
-
-# 소켓을 닫음.
-client_socket.close()
 server_socket.close()
