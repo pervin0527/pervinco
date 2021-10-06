485a53e9fdb33ac5eb1975ad5cef8e92c349ec89
pervin0527 <zfbtldnz@gmail.com>
2021-10-06 11:18:02 +0900

    updated 2021.10.06

--- source/7.python_server_client/tcp_client.py
+++ source/7.python_server_client/tcp_client.py
@@ -2,20 +2,43 @@ import cv2
 import socket
 import numpy as np
 
+###############################################################################################
+cap = cv2.VideoCapture(-1)
+MJPG_CODEC = 1196444237.0 # MJPG
+cap_AUTOFOCUS = 0
+cap_FOCUS = 0
+#cap_ZOOM = 400
+
+frame_width = int(640)
+frame_height = int(480)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
+# cv2.namedWindow('inference', cv2.WINDOW_FREERATIO)
+# cv2.resizeWindow('inference', frame_width, frame_height)
+
+cap.set(cv2.CAP_PROP_BRIGHTNESS, 100)
+# cap.set(cv2.CAP_PROP_FOURCC, MJPG_CODEC)
+cap.set(cv2.CAP_PROP_AUTOFOCUS, cap_AUTOFOCUS)
+cap.set(cv2.CAP_PROP_FOCUS, cap_FOCUS)
+##############################################################################################
+
 HOST = 'localhost'
-PORT = 8888
+PORT = 7777
   
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((HOST, PORT))
 
-image_path = "/data/Datasets/testset/ETRI_cropped_large/test_sample_04.jpg"
-image = cv2.imread(image_path)
-image = cv2.resize(image, (416, 416))
-encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
-result, imgencode = cv2.imencode('.jpg', image, encode_param)
-data = np.array(imgencode)
-stringData = data.tobytes()
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        print("nbbbbbbb")
+        break
 
-client_socket.send(str(len(stringData)).ljust(16).encode())
-client_socket.send(stringData)
-client_socket.close()
+    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+    result, imgencode = cv2.imencode('.jpg', frame, encode_param)
+    data = np.array(imgencode)
+    stringData = data.tobytes()
+
+    client_socket.send(str(len(stringData)).ljust(16).encode())
+    client_socket.send(stringData)
+    # client_socket.close()
--- source/7.python_server_client/tcp_server.py
+++ source/7.python_server_client/tcp_server.py
@@ -1,6 +1,8 @@
 import cv2
 import socket
 import numpy as np
+import darknet
+from ctypes import *
 
 def recvall(sock, count):
     buf = b''
@@ -15,21 +17,40 @@ def recvall(sock, count):
 
     return buf
 
+weight_file = "/data/Models/etri_yolov4/yolov4_final.weights"
+config_file = "/home/barcelona/darknet/custom/etri/deploy/yolov4.cfg"
+data_file = "/home/barcelona/darknet/custom/etri/data/etri.data"
+thresh_hold = .4
+
+network, class_names, class_colors = darknet.load_network(config_file, data_file, weight_file, batch_size=1)
+
+width = darknet.network_width(network)
+height = darknet.network_height(network)
+darknet_image = darknet.make_image(width, height, 3)
+
 HOST = 'localhost'
-PORT = 8888
-  
+PORT = 7777
+
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((HOST, PORT))
 server_socket.listen(True)
 
-connect, address = server_socket.accept()
+while True:  
+    connect, address = server_socket.accept()
+    
+    length = recvall(connect, 16)
+    stringData = recvall(connect, int(length))
+    data = np.frombuffer(stringData, dtype='uint8')
+    image = cv2.imdecode(data, 1)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = cv2.resize(image, (width, height))
 
-length = recvall(connect, 16)
-stringData = recvall(connect, int(length))
-data = np.frombuffer(stringData, dtype='uint8')
+    darknet.copy_image_from_bytes(darknet_image, image.tobytes())
+    detections = darknet.detect_image(network, class_names, darknet_image, thresh=thresh_hold)
+    image = darknet.draw_boxes(detections, image, class_colors)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-data = cv2.imdecode(data, 1)
-cv2.imshow('test', data)
-cv2.waitKey(0)
+    cv2.imshow('test', image)
+    cv2.waitKey(1)
 
-server_socket.close()
+# server_socket.close()
