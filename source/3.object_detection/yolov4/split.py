2aabfcf26726a81e16decc7e14dea11f70f48e6c
pervin0527 <zfbtldnz@gmail.com>
2021-10-08 11:24:53 +0900

    updated 2021.10.08

--- source/3.object_detection/yolov4/split.py
+++ source/3.object_detection/yolov4/split.py
@@ -4,16 +4,17 @@ import argparse
 parser = argparse.ArgumentParser(description='Dataset split train/test')
 parser.add_argument('--input_images_path', type=str)
 parser.add_argument('--train_file_name', type=str, default='train.txt')
-parser.add_argument('--test_file_name', type=str, default='test.txt')
+# parser.add_argument('--test_file_name', type=str, default='test.txt')
+parser.add_argument('--output_path', type=str)
 args = parser.parse_args()
 
 dataset_path = args.input_images_path
 splited_path = dataset_path.split('/')[:-1]
-output_path = './build/darknet/x64/data/'
+output_path = args.output_path
 print(output_path)
 
 file_train = open(output_path + '/' + args.train_file_name, 'w')  
-file_test = open(output_path + '/' + args.test_file_name, 'w')  
+# file_test = open(output_path + '/' + args.test_file_name, 'w')  
 
 # Percentage of images to be used for the test set
 percentage_test = 10
@@ -24,9 +25,10 @@ print(index_test)
 for pathAndFilename in glob.iglob(os.path.join(dataset_path, "*.jpg")):  
     title, ext = os.path.splitext(os.path.basename(pathAndFilename))
 
-    if counter == index_test+1:
-        counter = 1
-        file_test.write(dataset_path + "/" + title + '.jpg' + "\n")
-    else:
-        file_train.write(dataset_path + "/" + title + '.jpg' + "\n")
-        counter = counter + 1
+    # if counter == index_test+1:
+    #     counter = 1
+    #     file_test.write(dataset_path + "/" + title + '.jpg' + "\n")
+    # else:
+    #     file_train.write(dataset_path + "/" + title + '.jpg' + "\n")
+    #     counter = counter + 1
+    file_train.write(dataset_path + "/" + title + '.jpg' + "\n")
