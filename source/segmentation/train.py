ff3576c037b14b0db19cceb3bc7a4823525c530d
pervin0527 <zfbtldnz@gmail.com>
2022-05-11 12:18:50 +0900

    updated 2022.05.11

--- source/segmentation/train.py
+++ source/segmentation/train.py
@@ -177,6 +177,19 @@ def build_lrfn(lr_start=0.00001, lr_max=0.00005, lr_min=0.00001, lr_rampup_epoch
     return lrfn
 
 
+def focal_loss(gamma=2., alpha=.25):
+  def focal_loss_fixed(y_true, y_pred):
+    pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
+    pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
+
+    pt_1 = K.clip(pt_1, 1e-3, .999)
+    pt_0 = K.clip(pt_0, 1e-3, .999)
+
+    return -K.sum(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1))-K.sum((1-alpha) * K.pow( pt_0, gamma) * K.log(1. - pt_0))
+  
+  return focal_loss_fixed
+
+
 class DisplayCallback(tf.keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs=None):
         clear_output(wait=True)
@@ -197,7 +210,7 @@ if __name__ == "__main__":
     EPOCHS = 300
     IMG_SIZE = 320
     ES_PATIENT = 10
-    LEARNING_RATE = 0.0008 # Xception = 0.00005, ResNet50 = 0.00001
+    LEARNING_RATE = 0.00005 # Xception = 0.00005, ResNet50 = 0.00001
     
     CATEGORICAL = True
     BACKBONE_TRAINABLE = True
@@ -256,13 +269,14 @@ if __name__ == "__main__":
 
     if CATEGORICAL:
         # loss = tf.keras.losses.CategoricalCrossentropy()
-        loss = tfa.losses.SigmoidFocalCrossEntropy(from_logits=True, alpha=0.25, gamma=2)
+        # loss = tfa.losses.SigmoidFocalCrossEntropy(from_logits=True, alpha=0.25, gamma=2)
+        loss = focal_loss()
 
     else:
         loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 
     # model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE), loss=loss, metrics=["accuracy"])
-    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE), loss=loss, metrics=[tf.keras.metrics.MeanIoU(num_classes=NUM_CLASSES)])
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE), loss=loss, metrics=[tf.keras.metrics.OneHotMeanIoU(num_classes=NUM_CLASSES)])
 
     TRAIN_STEPS_PER_EPOCH = int(tf.math.ceil(len(train_images) / BATCH_SIZE).numpy())
     VALID_STEPS_PER_EPOCH = int(tf.math.ceil(len(valid_images) / BATCH_SIZE).numpy())
