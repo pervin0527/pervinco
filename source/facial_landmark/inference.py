e95f2bb6492a99ed8e0efebfc810221e50e6950c
pervin0527 <zfbtldnz@gmail.com>
2022-06-21 18:28:49 +0900

    updated 2022.06.21

--- source/facial_landmark/inference.py
+++ source/facial_landmark/inference.py
@@ -28,54 +28,54 @@ else:
 
 def get_overlay(index, image, landmarks):
     for (x, y) in landmarks:
-        cv2.circle(image, (int(x), int(y)), radius=1, color=(0, 0, 255), thickness=-1)
+        cv2.circle(image, (int(x), int(y)), radius=1, color=(0, 0, 255), thickness=1)
 
     return image
 
 
 def video_inference(video_capture_index):
     capture = cv2.VideoCapture(video_capture_index)
-
+    
     while cv2.waitKey(33) != ord('q'):
         ret, frame = capture.read()
+        frame = cv2.resize(frame, (224, 224))
         height, width = frame.shape[:2]
-        # height, width = input_shape[0], input_shape[1]
 
         resized_image = cv2.resize(frame, (input_shape[0], input_shape[1]))
         input_tensor = np.expand_dims(resized_image, axis=0)
         input_tensor = input_tensor / 255.0
 
-        prediction = model.predict(input_tensor)
-        pred_landmarks = prediction * input_shape[0]
-        pred_landmarks[0::2] = pred_landmarks[0::2] * width / input_shape[0]
-        pred_landmarks[1::2] = pred_landmarks[1::2] * height / input_shape[0]
-        pred_landmarks = pred_landmarks.reshape(-1, 2)
+        prediction = model.predict(input_tensor)[0]
+        landmarks = prediction * input_shape[0]
+        landmarks[0::2] = landmarks[0::2] * width / input_shape[0]
+        landmarks[1::2] = landmarks[1::2] * height / input_shape[0]
+        landmarks = landmarks.reshape(-1, 2)
 
-        # result_image = resized_image.copy()
-        # result_image = get_overlay(index=0, image=result_image, landmarks=pred_landmarks)
-        result_image = get_overlay(index=0, image=frame, landmarks=pred_landmarks)
+        result_image = get_overlay(index=0, image=frame, landmarks=landmarks)
+        result_image = cv2.resize(result_image, (640, 480))
         cv2.imshow("result", result_image)
 
     capture.release()
     cv2.destroyAllWindows()
 
 
-def image_inference(image_file):
-    image = cv2.imread(image_file)
-    height, width = image.shape[:2]
-    resized_image = cv2.resize(image, (input_shape[0], input_shape[1]))
-    input_tensor = np.expand_dims(resized_image, axis=0)
-    input_tensor = input_tensor / 255.0
+def image_inference(dir):
+    for index, file in enumerate(sorted(glob(f"{dir}/*"))):
+        image = cv2.imread(file)
+        height, width = image.shape[:2]
+        resized_image = cv2.resize(image, (input_shape[0], input_shape[1]))
+        input_tensor = np.expand_dims(resized_image, axis=0)
+        input_tensor = input_tensor / 255.0
 
-    pred = model.predict(input_tensor)
-    pred = pred * input_shape[0]
-    pred[0::2] = pred[0::2] * width / input_shape[0]
-    pred[1::2] = pred[1::2] * height / input_shape[0]
-    pred = pred.reshape(-1, 2)
+        prediction = model.predict(input_tensor)[0]
+        landmarks = prediction * input_shape[0]
+        landmarks[0::2] = landmarks[0::2] * width / input_shape[0]
+        landmarks[1::2] = landmarks[1::2] * height / input_shape[0]
+        landmarks = landmarks.reshape(-1, 2)
 
-    result_image = get_overlay(index=0, image=image, landmarks=pred)
-    cv2.imshow("result", result_image)
-    cv2.waitKey(0)
+        result_image = get_overlay(index=0, image=image, landmarks=landmarks)
+        cv2.imshow("result", result_image)
+        cv2.waitKey(0)
 
 
 def model_load(model_path):
@@ -89,5 +89,6 @@ if __name__ == "__main__":
     ckpt_path = "/data/Models/facial_landmark/best.h5"
     
     model = model_load(ckpt_path)
-    # video_inference("/data/Datasets/WFLW/inference_data/test01.mp4")
-    image_inference("/data/Datasets/WFLW/inference_data/1.png")
+    # video_inference(-1)
+    # video_inference("/data/Datasets/WFLW/inference_data/test02.mp4")
+    image_inference("/data/Datasets/WFLW/test_data/imgs")
