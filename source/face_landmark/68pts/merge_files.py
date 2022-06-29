3e01092b3a3997ce912dfc840bf2ccd4f8c75cd9
pervin0527 <zfbtldnz@gmail.com>
2022-06-29 09:39:18 +0900

    updated 2022.06.29

--- source/face_landmark/68pts/merge_files.py
+++ source/face_landmark/68pts/merge_files.py
@@ -1,15 +1,13 @@
 #-*- coding: utf-8 -*-
 import os
-import numpy as np
-import cv2
 import shutil
-
 
 def main(root_dir):
     files = ['WFLW/train_data_68pts/list.txt', '300VW_Dataset_2015_12_14/train_data_68pts/list.txt']
     dst_dir = os.path.join(root_dir, 'TOTAL_FACE/train_data_68pts')
     if not os.path.exists(dst_dir):
         os.makedirs(dst_dir)
+        os.makedirs(f"{dst_dir}/imgs")
     dst_path = os.path.join(dst_dir, 'list.txt')
     
     with open(dst_path, 'w') as fw:
@@ -19,13 +17,18 @@ def main(root_dir):
                 with open(fp, 'r') as fr:
                     lines = fr.readlines()
                     for index, line in enumerate(lines):
+                        image_path = line.split()[0]
+                        image_file_name = image_path.split('/')[-1]
+                        shutil.copyfile(f"{image_path}", f"{dst_dir}/imgs/{image_file_name}")
                         fw.write(line)
     
     test_files = ['WFLW/test_data_68pts/list.txt']
     test_dst_dir = os.path.join(root_dir, 'TOTAL_FACE/test_data_68pts')
     if not os.path.exists(test_dst_dir):
         os.makedirs(test_dst_dir)
+        os.makedirs(f"{test_dst_dir}/imgs")
     test_dst_path = os.path.join(test_dst_dir, 'list.txt')
+
     with open(test_dst_path, 'w') as fw:
         for file_path in test_files:
             fp = os.path.join(root_dir, file_path)
@@ -33,8 +36,11 @@ def main(root_dir):
                 with open(fp, 'r') as fr:
                     lines = fr.readlines()
                     for index, line in enumerate(lines):
+                        image_path = line.split()[0]
+                        image_file_name = image_path.split('/')[-1]
+                        shutil.copyfile(f"{image_path}", f"{test_dst_dir}/imgs/{image_file_name}")
                         fw.write(line)
-
+                        
 
 if __name__ == '__main__':
     root_dir = "/data/Datasets/"
