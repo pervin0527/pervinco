ba2980878af5e093b1cecb8620f8df70f5942b8e
pervinco <zfbtldnz@gmail.com>
2022-08-17 10:03:30 +0000

    updated 2022.08.17

--- source/object_detection/centernet/model.py
+++ source/object_detection/centernet/model.py
@@ -1,113 +1,185 @@
-import numpy as np
 import tensorflow as tf
-from loss import compute_loss
-# from backbone import resnet18, resnet101
+from loss import centernet_loss
 from keras_resnet import models as resnet_models
+from tensorflow.keras import backend as K
 
 def nms(heat, kernel=3):
-    hmax = tf.keras.layers.MaxPooling2D((kernel, kernel), strides=1, padding='same')(heat)
+    hmax = tf.nn.max_pool2d(heat, (kernel, kernel), strides=1, padding='SAME')
     heat = tf.where(tf.equal(hmax, heat), heat, tf.zeros_like(heat))
-
     return heat
 
 
-def topk(hm, max_detections):
+def topk(hm, max_objects=100):
     hm = nms(hm)
+    # (b, h * w * c)
     b, h, w, c = tf.shape(hm)[0], tf.shape(hm)[1], tf.shape(hm)[2], tf.shape(hm)[3]
+    # hm2 = tf.transpose(hm, (0, 3, 1, 2))
+    # hm2 = tf.reshape(hm2, (b, c, -1))
     hm = tf.reshape(hm, (b, -1))
-    scores, indices = tf.math.top_k(hm, k=max_detections, sorted=True)
-
+    # (b, k), (b, k)
+    scores, indices = tf.nn.top_k(hm, k=max_objects)
+    # scores2, indices2 = tf.nn.top_k(hm2, k=max_objects)
+    # scores2 = tf.reshape(scores2, (b, -1))
+    # topk = tf.nn.top_k(scores2, k=max_objects)
     class_ids = indices % c
     xs = indices // c % w
     ys = indices // c // w
     indices = ys * w + xs
-    
     return scores, indices, class_ids, xs, ys
 
 
-def decode(hm, wh, reg, max_detections):
-    scores, indices, class_ids, xs, ys = topk(hm, max_detections=max_detections)
+def evaluate_batch_item(batch_item_detections, num_classes, max_objects_per_class=20, max_objects=100,
+                        iou_threshold=0.5, score_threshold=0.1):
+    batch_item_detections = tf.boolean_mask(batch_item_detections,
+                                            tf.greater(batch_item_detections[:, 4], score_threshold))
+    detections_per_class = []
+    for cls_id in range(num_classes):
+        class_detections = tf.boolean_mask(batch_item_detections, tf.equal(batch_item_detections[:, 5], cls_id))
+        nms_keep_indices = tf.image.non_max_suppression(class_detections[:, :4],
+                                                        class_detections[:, 4],
+                                                        max_objects_per_class,
+                                                        iou_threshold=iou_threshold)
+        class_detections = K.gather(class_detections, nms_keep_indices)
+        detections_per_class.append(class_detections)
+
+    batch_item_detections = K.concatenate(detections_per_class, axis=0)
+
+    def filter():
+        nonlocal batch_item_detections
+        _, indices = tf.nn.top_k(batch_item_detections[:, 4], k=max_objects)
+        batch_item_detections_ = tf.gather(batch_item_detections, indices)
+        return batch_item_detections_
+
+    def pad():
+        nonlocal batch_item_detections
+        batch_item_num_detections = tf.shape(batch_item_detections)[0]
+        batch_item_num_pad = tf.maximum(max_objects - batch_item_num_detections, 0)
+        batch_item_detections_ = tf.pad(tensor=batch_item_detections,
+                                        paddings=[
+                                            [0, batch_item_num_pad],
+                                            [0, 0]],
+                                        mode='CONSTANT',
+                                        constant_values=0.0)
+        return batch_item_detections_
+
+    batch_item_detections = tf.cond(tf.shape(batch_item_detections)[0] >= 100,
+                                    filter,
+                                    pad)
+    return batch_item_detections
+
+
+def decode(hm, wh, reg, max_objects=100, nms=True, flip_test=False, num_classes=20, score_threshold=0.1):
+    if flip_test:
+        hm = (hm[0:1] + hm[1:2, :, ::-1]) / 2
+        wh = (wh[0:1] + wh[1:2, :, ::-1]) / 2
+        reg = reg[0:1]
+    scores, indices, class_ids, xs, ys = topk(hm, max_objects=max_objects)
     b = tf.shape(hm)[0]
-    
-    reg = tf.reshape(reg, [b, -1, 2])
-    wh = tf.reshape(wh, [b, -1, 2])
-    length = tf.shape(wh)[1]
-
-    batch_idx = tf.expand_dims(tf.range(0, b), 1)
-    batch_idx = tf.tile(batch_idx, (1, max_detections))
-    full_indices = tf.reshape(batch_idx, [-1]) * tf.cast(length, tf.int32) + tf.reshape(indices, [-1])
-                    
-    topk_reg = tf.gather(tf.reshape(reg, [-1,2]), full_indices)
-    topk_reg = tf.reshape(topk_reg, [b, -1, 2])
-    
-    topk_wh = tf.gather(tf.reshape(wh, [-1,2]), full_indices)
-    topk_wh = tf.reshape(topk_wh, [b, -1, 2])
-
+    # (b, h * w, 2)
+    reg = tf.reshape(reg, (b, -1, tf.shape(reg)[-1]))
+    # (b, h * w, 2)
+    wh = tf.reshape(wh, (b, -1, tf.shape(wh)[-1]))
+    # (b, k, 2)
+    topk_reg = tf.gather(reg, indices, batch_dims=1)
+    # (b, k, 2)
+    topk_wh = tf.cast(tf.gather(wh, indices, batch_dims=1), tf.float32)
     topk_cx = tf.cast(tf.expand_dims(xs, axis=-1), tf.float32) + topk_reg[..., 0:1]
     topk_cy = tf.cast(tf.expand_dims(ys, axis=-1), tf.float32) + topk_reg[..., 1:2]
-
-    topk_x1, topk_y1 = topk_cx - topk_wh[..., 0:1] / 2, topk_cy - topk_wh[..., 1:2] / 2
-    topk_x2, topk_y2 = topk_cx + topk_wh[..., 0:1] / 2, topk_cy + topk_wh[..., 1:2] / 2
-    
     scores = tf.expand_dims(scores, axis=-1)
     class_ids = tf.cast(tf.expand_dims(class_ids, axis=-1), tf.float32)
-
+    topk_x1 = topk_cx - topk_wh[..., 0:1] / 2
+    topk_x2 = topk_cx + topk_wh[..., 0:1] / 2
+    topk_y1 = topk_cy - topk_wh[..., 1:2] / 2
+    topk_y2 = topk_cy + topk_wh[..., 1:2] / 2
+    # (b, k, 6)
     detections = tf.concat([topk_x1, topk_y1, topk_x2, topk_y2, scores, class_ids], axis=-1)
-
+    if nms:
+        detections = tf.map_fn(lambda x: evaluate_batch_item(x[0],
+                                                             num_classes=num_classes,
+                                                             score_threshold=score_threshold),
+                               elems=[detections],
+                               dtype=tf.float32)
     return detections
-    
 
-def CenterNet(input_shape, num_classes, max_detections, backbone='resnet18'):
-    output_size = input_shape[0] // 4
-    image_input = tf.keras.Input(shape=(None, None, 3))
-    hm_input = tf.keras.Input(shape=(output_size, output_size, num_classes))
-    wh_input = tf.keras.Input(shape=(max_detections, 2))
-    reg_input = tf.keras.Input(shape=(max_detections, 2))
-    reg_mask_input = tf.keras.Input(shape=(max_detections,))
-    index_input = tf.keras.Input(shape=(max_detections,))
-    
-    if backbone == "resnet18":
-        resnet = resnet_models.ResNet18(image_input, include_top=False, freeze_bn=False)
+def centernet(inputs, num_classes, max_detections, threshold, backbone_name="resnet18", freeze_bn=True):
+    inputs = tf.keras.Input(shape=inputs)
 
-    elif backbone == "resnet101":
-        resnet = resnet_models.ResNet101(image_input, include_top=False, freeze_bn=False)
+    if backbone_name == "resnet18":
+       backbone_model = resnet_models.ResNet18(inputs, include_top=False, freeze_bn=freeze_bn)
+    elif backbone_name == 'resnet34':
+        backbone_model = resnet_models.ResNet34(inputs, include_top=False, freeze_bn=freeze_bn)
+    elif backbone_name == 'resnet50':
+        backbone_model = resnet_models.ResNet50(inputs, include_top=False, freeze_bn=freeze_bn)
+    elif backbone_name == 'resnet101':
+        backbone_model = resnet_models.ResNet101(inputs, include_top=False, freeze_bn=freeze_bn)
 
-    C5 = resnet.output[-1]
+    C5 = backbone_model.outputs[-1]
     x = tf.keras.layers.Dropout(rate=0.5)(C5)
-    
-    # decoder
+
     num_filters = 256
     for i in range(3):
         num_filters = num_filters // pow(2, i)
-        x = tf.keras.layers.Conv2DTranspose(num_filters, (4, 4), strides=2, use_bias=False, padding='same',
-                                            kernel_initializer='he_normal',
-                                            kernel_regularizer=tf.keras.regularizers.L2(5e-4))(x)
-        x = tf.keras.layers.BatchNormalization()(x)
-        x = tf.keras.layers.ReLU()(x)
 
-    # hm header
-    y1 = tf.keras.layers.Conv2D(64, 3, padding='same', use_bias=False, kernel_initializer='he_normal', kernel_regularizer=tf.keras.regularizers.L2(5e-4))(x)
+        x = tf.keras.layers.Conv2DTranspose(num_filters, (4, 4), strides=2, padding="same", kernel_initializer="he_normal", kernel_regularizer=tf.keras.regularizers.L2(5e-4), use_bias=False)(x)
+        x = tf.keras.layers.BatchNormalization()(x)
+        x = tf.nn.relu(x)
+
+    y1 = tf.keras.layers.Conv2D(64, 3, padding="SAME", use_bias=False, kernel_initializer="he_normal", kernel_regularizer=tf.keras.regularizers.L2(5e-4))(x)
     y1 = tf.keras.layers.BatchNormalization()(y1)
-    y1 = tf.keras.layers.ReLU()(y1)
+    y1 = tf.nn.relu(y1)
     y1 = tf.keras.layers.Conv2D(num_classes, 1, kernel_initializer='he_normal', kernel_regularizer=tf.keras.regularizers.L2(5e-4), activation='sigmoid')(y1)
 
-    # wh header
     y2 = tf.keras.layers.Conv2D(64, 3, padding='same', use_bias=False, kernel_initializer='he_normal', kernel_regularizer=tf.keras.regularizers.L2(5e-4))(x)
     y2 = tf.keras.layers.BatchNormalization()(y2)
     y2 = tf.keras.layers.ReLU()(y2)
     y2 = tf.keras.layers.Conv2D(2, 1, kernel_initializer='he_normal', kernel_regularizer=tf.keras.regularizers.L2(5e-4))(y2)
 
-    # reg header
     y3 = tf.keras.layers.Conv2D(64, 3, padding='same', use_bias=False, kernel_initializer='he_normal', kernel_regularizer=tf.keras.regularizers.L2(5e-4))(x)
     y3 = tf.keras.layers.BatchNormalization()(y3)
     y3 = tf.keras.layers.ReLU()(y3)
     y3 = tf.keras.layers.Conv2D(2, 1, kernel_initializer='he_normal', kernel_regularizer=tf.keras.regularizers.L2(5e-4))(y3)
 
-    loss_ = tf.keras.layers.Lambda(compute_loss, name='centernet_loss')([y1, y2, y3, hm_input, wh_input, reg_input, reg_mask_input, index_input])
-    model = tf.keras.Model(inputs=[image_input, hm_input, wh_input, reg_input, reg_mask_input, index_input], outputs=[loss_])
+    model = tf.keras.Model(inputs=inputs, outputs=[y1, y2, y3])
 
-    # detections = decode(y1, y2, y3)
-    detections = tf.keras.layers.Lambda(lambda x: decode(*x, max_detections=max_detections))([y1, y2, y3])
-    prediction_model = tf.keras.Model(inputs=image_input, outputs=detections)
+    # detections = tf.keras.layers.Lambda(lambda x: decode(*x, max_objects=max_detections, score_threshold=threshold, nms=nms, num_classes=num_classes))([y1, y2, y3])
+    # model = tf.keras.Model(inputs=inputs, outputs=[y1, y2, y3, detections])
 
-    return model, prediction_model
+    return model
+
+class CenterNet(tf.keras.Model):
+    def __init__(self, inputs, num_classes, max_detections, threshold, backbone_name="resnet18", freeze_bn=True):
+        super(CenterNet, self).__init__()
+
+        self.centernet = centernet(inputs, num_classes, max_detections, threshold, backbone_name, freeze_bn)
+        self.loss_tracker = tf.keras.metrics.Mean(name="loss")
+        self.centernet.summary()
+
+    def call(self, x, training=False):
+        hm_pred, wh_pred, reg_pred = self.centernet(x)
+        return hm_pred, wh_pred, reg_pred
+        # hm_pred, wh_pred, reg_pred, detections = self.centernet(x)
+        # return hm_pred, wh_pred, reg_pred, detections
+
+    def train_step(self, data):
+        image, hm, wh, reg, reg_mask, indices = data["image"], data["hm"], data["wh"], data["reg"], data["reg_mask"], data["indices"]
+
+        with tf.GradientTape() as tape:
+            hm_pred, wh_pred, reg_pred = self(image, training=True)
+            # hm_pred, wh_pred, reg_pred, detections = self(image, training=True)
+            loss = centernet_loss(hm_pred, wh_pred, reg_pred, hm, wh, reg, reg_mask, indices)
+        
+        trainable_vars = self.trainable_variables
+        gradients = tape.gradient(loss, trainable_vars)
+        self.optimizer.apply_gradients(zip(gradients, trainable_vars))
+        self.loss_tracker.update_state(loss)
+
+        return {"loss" : self.loss_tracker.result()}
+
+    def test_step(self, data):
+        image, hm, wh, reg, reg_mask, indices = data["image"], data["hm"], data["wh"], data["reg"], data["reg_mask"], data["indices"]
+        hm_pred, wh_pred, reg_pred = self(image, training=False)
+        # hm_pred, wh_pred, reg_pred, detections = self(image, training=False)
+        loss = centernet_loss(hm_pred, wh_pred, reg_pred, hm, wh, reg, reg_mask, indices)
+        self.loss_tracker.update_state(loss)
+
+        return {"loss" : self.loss_tracker.result()}
--- source/object_detection/centernet/train.py
+++ source/object_detection/centernet/train.py
@@ -1,14 +1,16 @@
 import os
+import sys
+import cv2
+import math
 import numpy as np
+import pandas as pd
 import tensorflow as tf
-from glob import glob
 from model import CenterNet
-from data_loader import DataGenerator
 from IPython.display import clear_output
+from tensorflow.keras import backend as K
 
-os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"]="0"
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+np.set_printoptions(threshold=sys.maxsize)
 gpus = tf.config.experimental.list_physical_devices('GPU')
 if len(gpus) > 1:
     try:
@@ -27,22 +29,149 @@ else:
     except RuntimeError as e:
         print(e)
 
+def gaussian_radius(det_size, min_overlap=0.7):
+    height, width = det_size
 
-def plot_predictions(model):
-    for idx, file in enumerate(sorted(glob("./samples/*"))):
-        file = tf.io.read_file(file)
-        image = tf.io.decode_jpeg(file, channels=3)
-        resized = tf.image.resize(image, (input_shape[0], input_shape[1]))
-        resized = (resized / 128.0) - 1
-        input_tensor = tf.expand_dims(resized, axis=0)
+    a1 = 1
+    b1 = (height + width)
+    c1 = width * height * (1 - min_overlap) / (1 + min_overlap)
+    sq1 = np.sqrt(b1 ** 2 - 4 * a1 * c1)
+    r1 = (b1 + sq1) / 2
 
-        prediction = pred_model.predict(input_tensor, verbose=0)[0]
-        scores = prediction[:, 4]
-        indexes = np.where(scores > 0.6)[0]
-        detections = prediction[indexes]
-        print(detections)
+    a2 = 4
+    b2 = 2 * (height + width)
+    c2 = (1 - min_overlap) * width * height
+    sq2 = np.sqrt(b2 ** 2 - 4 * a2 * c2)
+    r2 = (b2 + sq2) / 2
 
-        break
+    a3 = 4 * min_overlap
+    b3 = -2 * min_overlap * (height + width)
+    c3 = (min_overlap - 1) * width * height
+    sq3 = np.sqrt(b3 ** 2 - 4 * a3 * c3)
+    r3 = (b3 + sq3) / 2
+    return min(r1, r2, r3)
+
+
+def gaussian2D(shape, sigma=1):
+    m, n = [(ss - 1.) / 2. for ss in shape]
+    y, x = np.ogrid[-m:m + 1, -n:n + 1]
+
+    h = np.exp(-(x * x + y * y) / (2 * sigma * sigma))
+    h[h < np.finfo(h.dtype).eps * h.max()] = 0
+    return h
+
+def draw_gaussian(heatmap, center, radius, k=1):
+    diameter = 2 * radius + 1
+    gaussian = gaussian2D((diameter, diameter), sigma=diameter / 6)
+
+    x, y = int(center[0]), int(center[1])
+
+    height, width = heatmap.shape[0:2]
+
+    left, right = min(x, radius), min(width - x, radius + 1)
+    top, bottom = min(y, radius), min(height - y, radius + 1)
+
+    masked_heatmap = heatmap[y - top:y + bottom, x - left:x + right]
+    masked_gaussian = gaussian[radius - top:radius + bottom, radius - left:radius + right]
+    if min(masked_gaussian.shape) > 0 and min(masked_heatmap.shape) > 0:  # TODO debug
+        np.maximum(masked_heatmap, masked_gaussian * k, out=masked_heatmap)
+    return heatmap
+
+
+def image_preporcess(image, target_size, gt_boxes=None):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32)
+
+    ih, iw    = target_size
+    h,  w, _  = image.shape
+
+    scale = min(iw/w, ih/h)
+    nw, nh  = int(scale * w), int(scale * h)
+    image_resized = cv2.resize(image, (nw, nh))
+
+    image_paded = np.full(shape=[ih, iw, 3], fill_value=128.0, dtype=np.float32)
+    dw, dh = (iw - nw) // 2, (ih-nh) // 2
+    image_paded[dh:nh+dh, dw:nw+dw, :] = image_resized
+    image_paded = image_paded / 255.
+
+    if len(gt_boxes) > 0:
+        gt_boxes[:, [0,2]] = gt_boxes[:, [0,2]] * nw / iw + dw
+        gt_boxes[:, [1,3]] = gt_boxes[:, [1,3]] * nh / ih + dh
+        gt_boxes[:, 0:2][gt_boxes[:, 0:2]<0] = 0
+        gt_boxes[:, 2][gt_boxes[:, 2]>w] = w
+        gt_boxes[:, 3][gt_boxes[:, 3]>h] = h
+        box_w = gt_boxes[:, 2] - gt_boxes[:, 0]
+        box_h = gt_boxes[:, 3] - gt_boxes[:, 1]
+        gt_boxes = gt_boxes[np.logical_and(box_w>1, box_h>1)] # discard invalid box
+
+    return image_paded, gt_boxes
+
+
+def process_data(line):
+    line = line.decode("utf-8").split()
+    image = np.array(cv2.imread(line[0]))
+    labels = np.array([np.array(list(map(int,box.split(',')))) for box in line[1:]])
+
+    image, labels = image_preporcess(np.copy(image), [input_shape[0],input_shape[1]], np.copy(labels))
+
+    output_h = input_shape[0] // 4
+    output_w = input_shape[1] // 4
+    hm = np.zeros((output_h, output_w, len(classes)),dtype=np.float32)
+    wh = np.zeros((max_detections, 2),dtype=np.float32)
+    reg = np.zeros((max_detections, 2),dtype=np.float32)
+    ind = np.zeros((max_detections),dtype=np.float32)
+    reg_mask = np.zeros((max_detections),dtype=np.float32)
+
+    for idx, label in enumerate(labels):
+        bbox = label[:4]/4
+        class_id = label[4]
+        h, w = bbox[3] - bbox[1], bbox[2] - bbox[0]
+        radius = gaussian_radius((math.ceil(h),math.ceil(w)))
+        radius = max(0, int(radius))
+        ct = np.array([(bbox[0] + bbox[2]) / 2, (bbox[1] + bbox[3]) / 2], dtype=np.float32)
+        ct_int = ct.astype(np.int32)
+        draw_gaussian(hm[:,:,class_id], ct_int, radius)
+        wh[idx] = 1. * w, 1. * h
+        ind[idx] = ct_int[1] * output_w + ct_int[0]
+        reg[idx] = ct - ct_int
+        reg_mask[idx] = 1
+    
+    return image, hm, wh, reg, reg_mask, ind
+
+
+def get_data(batch_lines):
+    batch_image = np.zeros((batch_size, input_shape[0], input_shape[1], 3), dtype=np.float32)
+    batch_hm = np.zeros((batch_size, input_shape[0]//4, input_shape[1]//4, len(classes)), dtype=np.float32)
+    batch_wh = np.zeros((batch_size, max_detections, 2), dtype=np.float32)
+    batch_reg = np.zeros((batch_size, max_detections, 2), dtype=np.float32)
+    batch_reg_mask = np.zeros((batch_size, max_detections), dtype=np.float32)
+    batch_indices = np.zeros((batch_size, max_detections), dtype=np.float32)
+
+    for num, line in enumerate(batch_lines):
+        image, hm, wh, reg, reg_mask, indices = process_data(line)
+        batch_image[num, :, :, :] = image
+        batch_hm[num, :, :, :] = hm
+        batch_wh[num, :, :] = wh
+        batch_reg[num, :, :] = reg
+        batch_reg_mask[num, :] = reg_mask
+        batch_indices[num, :] = indices
+
+    return batch_image, batch_hm, batch_wh, batch_reg, batch_reg_mask, batch_indices
+
+
+def load_dataset(txt_path):
+    num_dataset = len(open(txt_path, 'r').readlines())
+    steps_per_epoch = int(math.ceil(float(num_dataset)) / batch_size)
+
+    dataset = tf.data.TextLineDataset(txt_path)
+    dataset = dataset.batch(batch_size)
+    dataset = dataset.map(lambda x : tf.numpy_function(get_data, inp=[x], 
+                                    Tout=[tf.float32, tf.float32, tf.float32, tf.float32, tf.float32, tf.float32]),
+                                    num_parallel_calls=tf.data.AUTOTUNE)
+    dataset = dataset.map(lambda image, hm, wh, reg, reg_mask, indices : {"image" : image, "hm" : hm, "wh" : wh, "reg" : reg, "reg_mask" : reg_mask, "indices" : indices})
+    dataset = dataset.repeat()
+    dataset = dataset.prefetch(tf.data.AUTOTUNE)                                    
+
+    return dataset, steps_per_epoch
 
 
 class DisplayCallback(tf.keras.callbacks.Callback):
@@ -51,63 +180,43 @@ class DisplayCallback(tf.keras.callbacks.Callback):
         plot_predictions(model=model)
 
 
+def plot_predictions(model):
+    image = cv2.imread(f"{data_dir}/dog.jpg")
+    image = cv2.resize(image, (input_shape[0], input_shape[1]))
+    input_tensor = np.expand_dims(image, axis=0)
+    hm_pred, wh_pred, reg_pred = model.predict(input_tensor)
+
+
 if __name__ == "__main__":
-    batch_size = 32
-    classes = ["face"]
-    max_detections = 10
-    input_shape = (512, 512, 3)
-
-    freeze_backbone = True
+    data_dir = "/home/ubuntu/Datasets/COCO2017"
+    label_dir = f"{data_dir}/Labels/labels.txt"
+    train_txt_dir = f"{data_dir}/train.txt"
+    test_txt_dir = f"{data_dir}/test.txt"
+    
     backbone = "resnet18"
-    train_dir = "/data/Datasets/WIDER/FACE/train_512"
-    test_dir = "/data/Datasets/WIDER/FACE/test_512"
-    save_dir = "/data/Models/FACE_DETECTION/test"
+    epochs = 1000
+    batch_size = 32
+    threshold = 0.1
+    max_detections = 100
+    input_shape = [512, 512, 3]
 
-    train_dataset = DataGenerator(train_dir, classes, batch_size, (input_shape[0], input_shape[1]), max_detections)
-    train_steps = int(tf.math.ceil(len(train_dataset) / batch_size).numpy())
+    df = pd.read_csv(label_dir, sep=",", index_col=False, header=None)
+    classes = df[0].to_list()
+    
+    train_dataset, train_steps = load_dataset(train_txt_dir)
+    test_dataset, test_steps = load_dataset(test_txt_dir)
 
-    test_dataset = DataGenerator(test_dir, classes, batch_size, (input_shape[0], input_shape[1]), max_detections)
-    test_steps = int(tf.math.ceil(len(test_dataset) / batch_size).numpy())
-
-    if freeze_backbone:
-        epochs = 100
-        learning_rate = 1e-3 # 0.001
-        ckpt_name = "freezed.h5"
-
-    else:
-        epochs = 500
-        learning_rate = 1e-4 # 0.0001
-        ckpt_name = "unfreezed.h5"
-
-    optimizer = tf.keras.optimizers.Adam(learning_rate)
+    optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
     callbacks = [
         DisplayCallback(),
-        tf.keras.callbacks.ReduceLROnPlateau(monitor="val_loss", patience=5, verbose=1, mode="min", factor=0.9, min_delta=0.01, min_lr=1e-6),
-        tf.keras.callbacks.EarlyStopping(monitor="val_loss", patience=10, mode="min"),
-        tf.keras.callbacks.TensorBoard(log_dir=f"{save_dir}/TensorBoard", update_freq='epoch'),
-        tf.keras.callbacks.ModelCheckpoint(f"{save_dir}/{ckpt_name}", monitor="val_loss", verbose=1, save_best_only=True, save_weights_only=True)
+        tf.keras.callbacks.ModelCheckpoint("/home/ubuntu/Models/test.h5", monitor="val_loss", verbose=1, save_best_only=True, save_weights_only=True)
     ]
 
-    with strategy.scope():
-        model, pred_model = CenterNet(input_shape, len(classes), max_detections, backbone)
-        model.compile(optimizer=optimizer, loss={'centernet_loss': lambda y_true, y_pred: y_pred})
-
-        if freeze_backbone:
-            if backbone == "resnet18":
-                freeze_layer = 85
-
-            elif backbone == "resnet101":
-                freeze_layer = 376
-
-            for i in range(freeze_layer): # resnet18 : 85, resnet101 : 376
-                model.layers[i].trainable = False
-
-        else:
-            model.load_weights("/data/Models/FACE_DETECTION/CenterNet-test/freezed.h5", by_name=True, skip_mismatch=True)
-    
+    model = CenterNet(input_shape, len(classes), max_detections, threshold, backbone, False)
+    model.compile(optimizer=optimizer)
     model.fit(train_dataset,
               steps_per_epoch=train_steps,
-              epochs = epochs,
               validation_data=test_dataset,
               validation_steps=test_steps,
-              callbacks=callbacks)
+              callbacks=callbacks,
+              epochs=epochs)
