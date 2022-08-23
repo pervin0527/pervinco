621a0d76e9cb0e80599abb156e1d72f89e24a618
pervinco <zfbtldnz@gmail.com>
2022-08-23 02:15:41 +0000

    updated 2022.08.23

--- source/SuperPoint/synthetic_shapes.py
+++ source/SuperPoint/synthetic_shapes.py
@@ -9,8 +9,7 @@ from data import synthetic_data
 
 
 def parse_primitives(names, all_primitives):
-    p = all_primitives if (names == 'all') \
-            else (names if isinstance(names, list) else [names])
+    p = all_primitives if (names == 'all')  else (names if isinstance(names, list) else [names])
     assert set(p) <= set(all_primitives)
     return p
 
