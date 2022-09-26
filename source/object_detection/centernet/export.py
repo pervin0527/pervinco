948ea3d46847b39b2a9d6cfd412f60b3435f76c8
pervin0527 <zfbtldnz@gmail.com>
2022-09-26 11:57:40 +0900

    updated 2022.09.26

--- source/object_detection/centernet/export.py
+++ source/object_detection/centernet/export.py
@@ -23,6 +23,19 @@ else:
     except RuntimeError as e:
         print(e)
 
+
+def build_model():
+    model = centernet([config["train"]["input_shape"][0], config["train"]["input_shape"][1], 3],
+                      num_classes,
+                      backbone=config["train"]["backbone"],
+                      max_objects=config["train"]["max_detection"],
+                      mode="predict")
+    model.load_weights(weight_path)
+    model.summary()
+
+    return model
+
+
 if __name__ == "__main__":
     config_path = "./configs/train.yaml"
     weight_path = "/home/ubuntu/Models/centernet/unfreeze.h5"
@@ -32,13 +45,20 @@ if __name__ == "__main__":
 
     class_names = read_label_file(config["path"]["label_path"])
     num_classes = len(class_names)
-    model = centernet([config["train"]["input_shape"][0], config["train"]["input_shape"][1], 3],
-                      num_classes,
-                      backbone=config["train"]["backbone"],
-                      max_objects=config["train"]["max_detection"],
-                      mode="predict")
+    model = build_model()
 
-    model.load_weights(weight_path)
-    model.summary()
+    run_model = tf.function(lambda x : model(x))
+    concrete_func = run_model.get_concrete_function(tf.TensorSpec([1, config["train"]["input_shape"][0], config["train"]["input_shape"][1], 3], model.inputs[0].dtype))
+    tf.saved_model.save(model, "/home/ubuntu/Models/centernet/saved_model", signatures=concrete_func)
+    print("Model SAVED")
 
-    
+    converter = tf.lite.TFLiteConverter.from_saved_model("/home/ubuntu/Models/centernet/saved_model")
+    print("Model LOADED")
+
+    converter.optimizations = [tf.lite.Optimize.DEFAULT]
+    converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS, tf.lite.OpsSet.SELECT_TF_OPS]
+    tflite_model = converter.convert()
+
+    with open("/home/ubuntu/Models/centernet/CenterNet.tflite", "wb") as f:
+        f.write(tflite_model)
+    print("Model CONVERTED")
