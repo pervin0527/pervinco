2d4ee438f537e8530fa7cad40737fc855bf95324
pervin0527 <zfbtldnz@gmail.com>
2022-11-21 09:19:00 +0000

    updated 2022.11.21(k-fold)

---
+++ source/augmentation/br/k-fold-augmentation.py
@@ -0,0 +1,310 @@
+import os
+import cv2
+import copy
+import random
+import numpy as np
+import albumentations as A
+
+from tqdm import tqdm
+from glob import glob
+from utils import make_file_list, load_annot_data, make_save_dir, annot_write
+
+
+def basic_augmentation(current_files):
+    idx = random.randint(0, len(current_files)-1)
+    image_file, annot_file = current_files[idx]
+    del current_files[idx]
+
+    image = cv2.imread(image_file)
+    bboxes, labels = load_annot_data(annot_file, target_classes=classes)
+
+    transformed = basic_transform(image=image, bboxes=bboxes, labels=labels)
+    aug_image, aug_bboxes, aug_labels = transformed["image"], transformed["bboxes"], transformed["labels"]
+
+    return aug_image, aug_bboxes, aug_labels
+
+
+def adjust_coordinates(bboxes):
+    ## remove too small bbox(object)
+    results = []
+    for box in bboxes:
+        if box[2] - box[0] < 5 or box[3] - box[1] < 5:
+            continue
+        results.append(box)
+    results = np.array(results)
+
+    return results
+
+
+def get_background_image(dirs):
+    mixup_file_list = []
+    for dir in dirs:
+        mixup_files = glob(f"{dir}/*.jpg")
+        mixup_file_list.extend(mixup_files)
+        
+    idx = random.randint(0, len(mixup_file_list)-1)
+    background_image = cv2.imread(mixup_file_list[idx])
+
+    return background_image
+
+
+def mixup_augmentation(fg_image, min=0.4, max=0.5, alpha=1.0):
+    background_transform = A.Compose([
+        A.OneOf([
+            A.RandomBrightnessContrast(brightness_limit=(-0.3, 0.3), p=0.4),
+            A.HueSaturationValue(hue_shift_limit=0, sat_shift_limit=(0, 0), val_shift_limit=(65, 100), p=0.4),
+        ], p=0.7),
+
+        A.MotionBlur(p=0.3),
+        A.Resize(height=fg_image.shape[0], width=fg_image.shape[1], p=1),
+    ])
+
+    lam = np.clip(np.random.beta(alpha, alpha), min, max)
+    bg_image = get_background_image(mixup_data_dir)
+    bg_transformed = background_transform(image=bg_image)
+    bg_image = bg_transformed["image"]
+
+    mixup_image = (lam * bg_image + (1 - lam) * fg_image).astype(np.uint8)
+
+    return mixup_image
+
+
+
+def crop_image(image, bboxes, labels, coordinates):
+    crop_transform = A.Compose([
+        A.OneOf([
+            A.RandomBrightnessContrast(brightness_limit=(-0.3, 0.3), p=0.4),
+            A.HueSaturationValue(hue_shift_limit=0, sat_shift_limit=(0, 0), val_shift_limit=(65, 100), p=0.4),
+            A.MotionBlur(p=0.2),
+        ], p=0.7),
+
+        A.OneOf([
+            A.RandomSizedBBoxSafeCrop(img_size, img_size, p=0.4),
+            A.CropAndPad(percent=0.2, pad_mode=0, keep_size=True, p=0.4),
+        ],p=0.6),
+
+        A.Resize(height=coordinates[3]-coordinates[1], width=coordinates[2]-coordinates[0], p=1),
+
+    ], bbox_params=A.BboxParams(format="pascal_voc", label_fields=["labels"]))
+    cropped = crop_transform(image=image, bboxes=bboxes, labels=labels)
+    crop_image, crop_bboxes, crop_labels = cropped["image"], cropped["bboxes"], cropped["labels"]
+
+    # if mixup and random.random() < mixup_prob:
+    #     crop_image = mixup_augmentation(crop_image, min=mixup_min, max=mixup_max)  
+
+    return crop_image, np.array(crop_bboxes), crop_labels
+
+
+def mosaic_augmentation(current_files):
+    mosaic_image = np.full((img_size, img_size, 3), 128, dtype=np.uint8)
+    mosaic_boxes, mosaic_labels = [], []
+
+    xc = int(random.uniform(img_size * 0.25, img_size * 0.75))
+    yc = int(random.uniform(img_size * 0.25, img_size * 0.75))
+    # indices = [random.randint(0, len(current_files)-1) for _ in range(4)]
+    indices = [0, 1, 2, 3]
+    for i, index in enumerate(indices):
+        image_file, annotation_file = current_files[index]
+        del current_files[index]
+
+        image = cv2.imread(image_file)
+        bboxes, labels = load_annot_data(annotation_file, target_classes=classes)
+        # bboxes = adjust_coordinates(bboxes)
+        bboxes = np.array(bboxes)
+
+        if i == 0:
+            image, bbox, label = crop_image(image, bboxes, labels, (img_size-xc, img_size-yc, img_size, img_size))
+            mosaic_image[0 : yc, 0 : xc, :] = image
+            mosaic_boxes.extend(bbox)
+            mosaic_labels.extend(label)
+
+        elif i == 1:
+            image, bbox, label = crop_image(image, bboxes, labels, (0, img_size-yc, img_size-xc, img_size))
+            mosaic_image[0 : yc, xc : img_size, :] = image
+
+            if bbox.shape[0] > 0:
+                bbox[:, [0, 2]] += xc
+
+            mosaic_boxes.extend(bbox)
+            mosaic_labels.extend(label)
+
+        elif i == 2:
+            image, bbox, label = crop_image(image, bboxes, labels, (0, 0, img_size-xc, img_size-yc))
+            mosaic_image[yc:img_size, xc:img_size, :] = image
+
+            if bbox.shape[0] > 0:
+                bbox[:, [0, 2]] += xc
+                bbox[:, [1, 3]] += yc
+
+            mosaic_boxes.extend(bbox)
+            mosaic_labels.extend(label)
+        
+        else:
+            image, bbox, label = crop_image(image, bboxes, labels, (img_size-xc, 0, img_size, img_size-yc))
+            mosaic_image[yc : img_size, 0 : xc, :] = image
+
+            if bbox.shape[0] > 0:
+                bbox[ :, [1, 3]] += yc
+
+            mosaic_boxes.extend(bbox)
+            mosaic_labels.extend(label)
+
+    return mosaic_image, mosaic_boxes, mosaic_labels
+
+        
+def train_augmentation(files, folds):
+    tmp = copy.deepcopy(files)
+    
+    for fold in range(folds):
+        save_path = f"{save_dir}/train/{fold}"
+        make_save_dir(save_path)
+
+        for number in tqdm(range(total_steps)):
+            if len(tmp) < 10:
+                print("update")
+                tmp = copy.deepcopy(files)
+
+            random.shuffle(tmp)
+            if mosaic and random.random() < mosaic_prob:
+                result_image, result_bboxes, result_labels = mosaic_augmentation(tmp)
+            else:
+                result_image, result_bboxes, result_labels = basic_augmentation(tmp)
+
+            if mixup and random.random() < mixup_prob:
+                result_image = mixup_augmentation(result_image, mixup_min, mixup_max)
+
+            cv2.imwrite(f"{save_path}/JPEGImages/{number:>09}.jpg", result_image)
+            annot_write(f"{save_path}/Annotations/{number:>09}.xml", result_bboxes, result_labels, result_image.shape[:2])
+
+            sample = result_image.copy()
+            for bbox in result_bboxes:
+                xmin, ymin, xmax, ymax = int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])
+                cv2.rectangle(sample, (xmin, ymin), (xmax, ymax), (0, 0, 255), 2)
+            cv2.imwrite(f"{save_path}/Results/{number:>09}.jpg", sample)
+
+            # cv2.imshow("sample", sample)
+            # cv2.waitKey(0)
+
+def valid_augmentation(files):
+    save_path = f"{save_dir}/valid"
+    make_save_dir(save_path)
+
+    for idx in tqdm(range(len(files))):
+        random.shuffle(files)
+        image_file, annot_file = files[idx]
+        image = cv2.imread(image_file)
+        bboxes, labels = load_annot_data(annot_file, target_classes=classes)
+
+        val_transformed = valid_transform(image=image, bboxes=bboxes, labels=labels)
+        val_image, val_bboxes, val_labels = val_transformed["image"], val_transformed["bboxes"], val_transformed["labels"]
+
+        cv2.imwrite(f"{save_path}/JPEGImages/{idx:>09}.jpg", val_image)
+        annot_write(f"{save_path}/Annotations/{idx:>09}.xml", val_bboxes, val_labels, val_image.shape[:2])
+
+        sample = val_image.copy()
+        for bbox in val_bboxes:
+            xmin, ymin, xmax, ymax = int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])
+            cv2.rectangle(sample, (xmin, ymin), (xmax, ymax), (0, 0, 255), 2)
+        cv2.imwrite(f"{save_path}/Results/{idx:>09}.jpg", sample)
+
+
+def add_nf_data(path, transform):
+    save_path = f"{save_dir}/train"
+    nf_files = []
+    ratio = int(nf_ratio * total_steps)
+
+    folders = glob(f"{path}/*")
+    for folder in folders:
+        files = glob(f"{folder}/*")
+        if len(files) > int(ratio / len(folders)):
+            files = random.sample(files, int(ratio / len(folders)))
+        nf_files.extend(files)
+
+    for idx in tqdm(range(len(nf_files))):
+        try:
+            nf_image = cv2.imread(nf_files[idx])
+            nf_image = cv2.resize(nf_image, (img_size, img_size))
+            nf_transform = transform(image=nf_image)
+
+            nf_t_image = nf_transform["image"]
+
+            cv2.imwrite(f"{save_path}/JPEGImages/NF_{idx}.jpg", nf_t_image)
+            annot_write(f"{save_path}/Annotations/NF_{idx}.xml", None, None, (img_size, img_size))
+
+            # cv2.imshow("NF", nf_t_image)
+            # cv2.waitKey(0)
+        except:
+            os.remove(nf_files[idx])
+
+
+if __name__ == "__main__":
+    data_dir = ["/data/Datasets/BR/seed1_384", "/data/Datasets/BR/br_train"]
+    save_dir = f"{data_dir[0]}/fold00"
+    total_steps = 32000
+    num_valid = 320
+    folds = 10
+    classes = ["Baskin_robbins"] # Baskin_robbins
+
+    img_size = 384
+    mosaic = True
+    mosaic_prob = 0.4
+    mixup = True
+    mixup_prob = 0.4
+    mixup_min = 0.1
+    mixup_max = 0.3
+    mixup_data_dir = ["/data/Datasets/VOCdevkit/VOC2012/JPEGImages"]
+
+    negative_false = True
+    nf_ratio = 0.2
+    nf_data_dir = "/data/Datasets/SPC/download"
+
+    basic_transform = A.Compose([
+        A.Resize(img_size, img_size, p=1),
+        
+        A.OneOf([
+            A.RandomSizedBBoxSafeCrop(img_size, img_size, p=0.4),
+            A.CropAndPad(percent=0.2, pad_mode=0, keep_size=True, p=0.4),
+        ],p=0.4),
+
+        A.OneOf([
+            A.RandomBrightnessContrast(brightness_limit=(-0.3, 0.3), p=0.5),
+            A.HueSaturationValue(hue_shift_limit=0, sat_shift_limit=(0, 0), val_shift_limit=(65, 100), p=0.5),
+        ], p=0.5),
+        
+        A.MotionBlur(p=0.3),
+    ], bbox_params=A.BboxParams(format="pascal_voc", label_fields=["labels"]))
+
+    valid_transform = A.Compose([
+        A.Resize(img_size, img_size, p=1),
+    ], bbox_params=A.BboxParams(format="pascal_voc", label_fields=["labels"]))
+
+    train_files, valid_files = make_file_list(data_dir, num_valid)
+    train_augmentation(train_files, folds)
+    valid_augmentation(valid_files)
+
+    if negative_false:
+        nf_transform = A.Compose([
+            A.OneOf([
+                A.Resize(img_size, img_size, p=0.35),
+                A.CropAndPad(percent=0.2, pad_mode=0, keep_size=True, p=0.35),
+                A.RandomCrop(img_size, img_size, p=0.3),
+            ], p=1),
+
+            A.OneOf([
+                A.HueSaturationValue(p=0.4),
+                A.RGBShift(p=0.3)
+            ]),
+
+            A.OneOf([
+                A.Downscale(0.25, 0.65, p=0.4, interpolation=cv2.INTER_NEAREST),
+                A.ISONoise(p=0.4),
+                A.MotionBlur(p=0.4)
+            ], p=0.45),
+
+            A.OneOf([
+                A.HorizontalFlip(p=0.3),
+                A.VerticalFlip(p=0.3),
+                A.RandomGridShuffle(p=0.3)
+            ], p=0.3)
+        ])
+        add_nf_data(nf_data_dir, nf_transform)
---
+++ source/object_detection/model-maker/k-fold-train.py
@@ -0,0 +1,172 @@
+import os
+import shutil
+import pandas as pd
+import tensorflow as tf
+import xml.etree.ElementTree as ET
+
+from tqdm import tqdm
+from glob import glob
+from tflite_model_maker import object_detector
+from tflite_model_maker.config import ExportFormat
+from tensorflow_examples.lite.model_maker.third_party.efficientdet.keras import train, train_lib
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if len(gpus) > 1:
+    try:
+        print("Activate Multi GPU")
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        strategy = tf.distribute.MirroredStrategy(cross_device_ops=tf.distribute.HierarchicalCopyAllReduce())
+    except RuntimeError as e:
+        print(e)
+
+else:
+    try:
+        print("Activate Sigle GPU")
+        tf.config.experimental.set_memory_growth(gpus[0], True)
+        strategy = tf.distribute.experimental.CentralStorageStrategy()
+    except RuntimeError as e:
+        print(e)
+
+
+def load_annot_data(annot_file, target_classes):
+    target = ET.parse(annot_file).getroot()
+
+    height = int(target.find('size').find('height').text)
+    width = int(target.find('size').find('width').text)
+
+    bboxes, labels = [], []
+    for obj in target.iter("object"):
+        label = obj.find("name").text.strip()
+        if label in target_classes:
+            labels.append([label])
+
+            bndbox = obj.find("bndbox")
+            bbox = []
+            for current in ["xmin", "ymin", "xmax", "ymax"]:
+                coordinate = int(float(bndbox.find(current).text))
+                if current == "xmin" and coordinate < 0:
+                    coordinate = 0
+                elif current == "ymin" and coordinate < 0:
+                    coordinate = 0
+                elif current == "xmax" and coordinate > width:
+                    coordinate = width
+                elif current == "ymax" and coordinate > height:
+                    coordinate = height
+                bbox.append(coordinate)
+            bboxes.append(bbox)
+
+    return bboxes, labels
+
+
+def label_check(dir):
+    files = sorted(glob(f"{dir}/Annotations/*.xml"))
+    targets = set()
+    for idx in tqdm(range(len(files))):
+        file = files[idx]
+        bboxes, labels = load_annot_data(file, CLASSES)
+        for label in labels:
+            targets.add(label[0])
+            if not label[0] in CLASSES:
+                print(file, label)
+                return False
+
+    return True, len(files)
+
+
+if __name__ == "__main__":
+    ROOT_DIR = "/data/Datasets/BR"
+    SAVE_PATH = "/data/Models/BR_FINAL"
+    TRAIN_DIR = f"{ROOT_DIR}/seed1_384/fold00/train"
+    VALID_DIR = f"{ROOT_DIR}/seed1_384/fold00/valid"
+    
+    LABEL_FILE = f"{ROOT_DIR}/Labels/labels.txt"
+    LABEL_FILE = pd.read_csv(LABEL_FILE, sep=',', index_col=False, header=None)
+    CLASSES = LABEL_FILE[0].tolist()
+    print(CLASSES)
+    
+    FOLDS = 10
+    EPOCHS = 100
+    BATCH_SIZE = 32 * len(gpus)
+    MAX_DETECTIONS = 10
+    HPARAMS = {
+        "optimizer" : "sgd",
+        "momentum" : 0.9,
+        "lr_decay_method" : "cosine",
+        "learning_rate" : 0.0005,
+        "lr_warmup_init" : 0.00005,
+        "lr_warmup_epoch" : 1.0,
+        "aspect_ratios" : [12.59, 7.38, 4.58, 2.74, 1.5, 0.73],
+        "alpha" : 0.25,
+        "gamma" : 2,
+        "max_instances_per_image" : 10
+    }
+
+    PROJECT = ROOT_DIR.split('/')[-1]
+    DS_NAME = TRAIN_DIR.split('/')[-2]
+    MODEL_FILE = f"{PROJECT}-{DS_NAME}-{EPOCHS}"
+
+    for f in range(FOLDS):
+        train_data = object_detector.DataLoader.from_pascal_voc(images_dir=f"{TRAIN_DIR}/{f}/JPEGImages",
+                                                                annotations_dir=f"{TRAIN_DIR}/{f}/Annotations", 
+                                                                label_map=CLASSES)
+
+        validation_data = object_detector.DataLoader.from_pascal_voc(images_dir=f'{VALID_DIR}/JPEGImages',
+                                                                    annotations_dir=f'{VALID_DIR}/Annotations',
+                                                                    label_map=CLASSES)
+
+        spec = object_detector.EfficientDetLite1Spec(verbose=1,
+                                                    strategy=None, # 'gpus', None
+                                                    hparams=HPARAMS,
+                                                    tflite_max_detections=MAX_DETECTIONS,
+                                                    model_dir=f'{SAVE_PATH}/{MODEL_FILE}')
+
+        detector = object_detector.create(train_data,
+                                          model_spec=spec,
+                                          epochs=EPOCHS,
+                                          batch_size=BATCH_SIZE,
+                                          validation_data=validation_data,
+                                          do_train=False,
+                                          train_whole_model=True)
+
+        train_ds, steps_per_epoch, _ = detector._get_dataset_and_steps(train_data, BATCH_SIZE, is_training=True)
+        validation_ds, validation_steps, val_json_file = detector._get_dataset_and_steps(validation_data, BATCH_SIZE, is_training=False)
+
+        with strategy.scope():
+            model = detector.create_model()
+            config = spec.config
+            config.update(
+                dict(
+                    steps_per_epoch=steps_per_epoch,
+                    eval_samples=BATCH_SIZE*validation_steps,
+                    val_json_file=val_json_file,
+                    batch_size=BATCH_SIZE
+                )
+            )
+            train.setup_model(model, config)
+            model.summary()
+
+            try:
+                latest = tf.train.latest_checkpoint(f"{SAVE_PATH}/")
+                last_epoch = int(latest.split('/')[-1].split("-")[1])
+                model.load_weights(latest)
+
+                print("Checkpoint found {}".format(latest))
+            except Exception as e:
+                print("Checkpoint not found: ", e)
+
+            model.fit(train_ds,
+                      initial_epoch=EPOCHS * f, 
+                      epochs=EPOCHS * (f+1),
+                      steps_per_epoch=steps_per_epoch,
+                      validation_data=validation_ds,
+                      validation_steps=validation_steps,
+                      callbacks=train_lib.get_callbacks(config.as_dict(), validation_ds))
+
+        detector.model = model
+        detector.export(export_dir=f"{SAVE_PATH}/{MODEL_FILE}",
+                        tflite_filename='model.tflite',
+                        saved_model_filename=f'{SAVE_PATH}/{MODEL_FILE}/saved_model',
+                        export_format=[ExportFormat.TFLITE, ExportFormat.SAVED_MODEL])
+        print("exported")
