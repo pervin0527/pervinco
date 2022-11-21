ad2d6823d3ea5b9f374cbf3b5ab0c26579f35f0f
pervin0527 <zfbtldnz@gmail.com>
2022-11-21 18:18:35 +0900

    updated 2022.11.21

--- source/object_detection/model-maker/load_export.py
+++ source/object_detection/model-maker/load_export.py
@@ -1,5 +1,4 @@
 import os
-import shutil
 import pandas as pd
 import tensorflow as tf
 import xml.etree.ElementTree as ET
@@ -90,94 +89,95 @@ if __name__ == "__main__":
     train_check, train_files = label_check(TRAIN_DIR)
     valid_check, valid_files = label_check(VALID_DIR)
     
-    SAVE_PATH = '/'.join(CKPT_PATH.split('/')[:-1])
-    EPOCHS = 400
-    BATCH_SIZE = 32 * len(gpus)
-    MAX_DETECTIONS = 10
-    HPARAMS = {
-        "optimizer" : "sgd",
-        "momentum" : 0.9,
-        "lr_decay_method" : "cosine",
-        "learning_rate" : 0.0005,
-        "lr_warmup_init" : 0.00005,
-        "lr_warmup_epoch" : 1.0,
-        "aspect_ratios" : [12.59, 7.38, 4.58, 2.74, 1.5, 0.73], ## [8.24, 4.42, 2.2, 0.92]
-        # "num_scales" : 4,
-        # "anchor_scale" : 5.0,
-        "alpha" : 0.25,
-        "gamma" : 2,
-        "max_instances_per_image" : 10
-    }
-
-    if IS_TRAIN:
-        PROJECT = ROOT_DIR.split('/')[-1]
-        DS_NAME = TRAIN_DIR.split('/')[-2]
-        MODEL_FILE = f"{PROJECT}-{DS_NAME}-{EPOCHS}"
-    else:
-        MODEL_FILE = CKPT_PATH.split('/')[-1] + "-export"
-
-    train_data = object_detector.DataLoader.from_pascal_voc(images_dir=f"{TRAIN_DIR}/JPEGImages",
-                                                            annotations_dir=f"{TRAIN_DIR}/Annotations", 
-                                                            label_map=CLASSES)
-
-    validation_data = object_detector.DataLoader.from_pascal_voc(images_dir=f'{VALID_DIR}/JPEGImages',
-                                                                    annotations_dir=f'{VALID_DIR}/Annotations',
-                                                                    label_map=CLASSES)
-
-    spec = object_detector.EfficientDetLite1Spec(verbose=1,
-                                                 strategy="gpus", # 'gpus', None
-                                                 hparams=HPARAMS,
-                                                 tflite_max_detections=MAX_DETECTIONS,
-                                                 model_dir=f'{SAVE_PATH}/{MODEL_FILE}')
-
-    detector = object_detector.create(train_data,
-                                      model_spec=spec,
-                                      epochs=EPOCHS,
-                                      batch_size=BATCH_SIZE,
-                                      validation_data=validation_data,
-                                      do_train=False,
-                                      train_whole_model=True)
-
-    train_ds, steps_per_epoch, _ = detector._get_dataset_and_steps(train_data, BATCH_SIZE, is_training=True)
-    validation_ds, validation_steps, val_json_file = detector._get_dataset_and_steps(validation_data, BATCH_SIZE, is_training=False)
-
-    with strategy.scope():
-        model = detector.create_model()
-        config = spec.config
-        config.update(
-            dict(
-                steps_per_epoch=steps_per_epoch,
-                eval_samples=BATCH_SIZE*validation_steps,
-                val_json_file=val_json_file,
-                batch_size=BATCH_SIZE
-            )
-        )
-        train.setup_model(model, config)
-        model.summary()
-
-        try:
-            latest = tf.train.latest_checkpoint(f"{CKPT_PATH}/")
-            last_epoch = int(latest.split('/')[-1].split("-")[1])
-            model.load_weights(latest)
-
-            print("Checkpoint found {}".format(latest))
-        except Exception as e:
-            print("Checkpoint not found: ", e)
+    if train_check and valid_check:
+        SAVE_PATH = '/'.join(CKPT_PATH.split('/')[:-1])
+        EPOCHS = 400
+        BATCH_SIZE = 32 * len(gpus)
+        MAX_DETECTIONS = 10
+        HPARAMS = {
+            "optimizer" : "sgd",
+            "momentum" : 0.9,
+            "lr_decay_method" : "cosine",
+            "learning_rate" : 0.0005,
+            "lr_warmup_init" : 0.00005,
+            "lr_warmup_epoch" : 1.0,
+            "aspect_ratios" : [12.59, 7.38, 4.58, 2.74, 1.5, 0.73], ## [8.24, 4.42, 2.2, 0.92]
+            # "num_scales" : 4,
+            # "anchor_scale" : 5.0,
+            "alpha" : 0.25,
+            "gamma" : 2,
+            "max_instances_per_image" : 10
+        }
 
         if IS_TRAIN:
-            model.fit(
-                train_ds,
-                initial_epoch=0, 
-                epochs=(EPOCHS - last_epoch),
-                steps_per_epoch=steps_per_epoch,
-                validation_data=validation_ds,
-                validation_steps=validation_steps,
-                callbacks=train_lib.get_callbacks(config.as_dict(), validation_ds)
-            )
+            PROJECT = ROOT_DIR.split('/')[-1]
+            DS_NAME = TRAIN_DIR.split('/')[-2]
+            MODEL_FILE = f"{PROJECT}-{DS_NAME}-{EPOCHS}"
+        else:
+            MODEL_FILE = CKPT_PATH.split('/')[-1] + "-export"
 
-    detector.model = model
-    detector.export(export_dir=f"{SAVE_PATH}/{MODEL_FILE}",
-                    tflite_filename='model.tflite',
-                    saved_model_filename=f'{SAVE_PATH}/{MODEL_FILE}/saved_model',
-                    export_format=[ExportFormat.TFLITE, ExportFormat.SAVED_MODEL])
-    print("exported")
+        train_data = object_detector.DataLoader.from_pascal_voc(images_dir=f"{TRAIN_DIR}/JPEGImages",
+                                                                annotations_dir=f"{TRAIN_DIR}/Annotations", 
+                                                                label_map=CLASSES)
+
+        validation_data = object_detector.DataLoader.from_pascal_voc(images_dir=f'{VALID_DIR}/JPEGImages',
+                                                                        annotations_dir=f'{VALID_DIR}/Annotations',
+                                                                        label_map=CLASSES)
+
+        spec = object_detector.EfficientDetLite1Spec(verbose=1,
+                                                    strategy="gpus", # 'gpus', None
+                                                    hparams=HPARAMS,
+                                                    tflite_max_detections=MAX_DETECTIONS,
+                                                    model_dir=f'{SAVE_PATH}/{MODEL_FILE}')
+
+        detector = object_detector.create(train_data,
+                                        model_spec=spec,
+                                        epochs=EPOCHS,
+                                        batch_size=BATCH_SIZE,
+                                        validation_data=validation_data,
+                                        do_train=False,
+                                        train_whole_model=True)
+
+        train_ds, steps_per_epoch, _ = detector._get_dataset_and_steps(train_data, BATCH_SIZE, is_training=True)
+        validation_ds, validation_steps, val_json_file = detector._get_dataset_and_steps(validation_data, BATCH_SIZE, is_training=False)
+
+        with strategy.scope():
+            model = detector.create_model()
+            config = spec.config
+            config.update(
+                dict(
+                    steps_per_epoch=steps_per_epoch,
+                    eval_samples=BATCH_SIZE*validation_steps,
+                    val_json_file=val_json_file,
+                    batch_size=BATCH_SIZE
+                )
+            )
+            train.setup_model(model, config)
+            model.summary()
+
+            try:
+                latest = tf.train.latest_checkpoint(f"{CKPT_PATH}/")
+                last_epoch = int(latest.split('/')[-1].split("-")[1])
+                model.load_weights(latest)
+
+                print("Checkpoint found {}".format(latest))
+            except Exception as e:
+                print("Checkpoint not found: ", e)
+
+            if IS_TRAIN:
+                model.fit(
+                    train_ds,
+                    initial_epoch=0, 
+                    epochs=(EPOCHS - last_epoch),
+                    steps_per_epoch=steps_per_epoch,
+                    validation_data=validation_ds,
+                    validation_steps=validation_steps,
+                    callbacks=train_lib.get_callbacks(config.as_dict(), validation_ds)
+                )
+
+        detector.model = model
+        detector.export(export_dir=f"{SAVE_PATH}/{MODEL_FILE}",
+                        tflite_filename='model.tflite',
+                        saved_model_filename=f'{SAVE_PATH}/{MODEL_FILE}/saved_model',
+                        export_format=[ExportFormat.TFLITE, ExportFormat.SAVED_MODEL])
+        print("exported")
